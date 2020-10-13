b55f59a0f790b0033224c6700fd4215a419e2e3c
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-10-13 17:36:01 +0200

    Database structure

---
+++ database/webots-cloud.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `url` varchar(2048) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `parent` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `stars` int(11) NOT NULL,
+  `language` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`);
+
+ALTER TABLE `project`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+COMMIT;
