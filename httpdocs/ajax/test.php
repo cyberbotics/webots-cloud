6247100dd673dc466be66f95c219f1ead57f6224
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-11-06 17:52:42 +0100

    Update test.php

--- httpdocs/ajax/test.php
+++ httpdocs/ajax/test.php
@@ -1,5 +1,5 @@
 <?php
-  $context = stream_context_create(['http' => ['method' => 'GET', 'header' => ['User-Agent: PHP']]]);
+  $context = stream_context_create(['http' => ['method' => 'GET', 'header' => ['User-Agent: PHP', '6f8ecd5f0734091246c6: f9037a2fe1617b4738e3a85edc1b4b6e6f7d0cad']]]);
   $info_json = file_get_contents("https://api.github.com/rate_limit", false, $context);
   // https://api.github.com/repos/cyberbotics/webots
   print($info_json);
