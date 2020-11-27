bc6ec2d99c70ab5334d0551f241eff1b5d5c6dc4
Olivier Michel <>
2020-11-27 12:53:17 +0100

    Fixed database creation

--- database/webots-cloud.sql
+++ database/webots-cloud.sql
@@ -1,11 +1,14 @@
 CREATE TABLE `server` (
   `id` int(11) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `url` varchar(2048) CHARACTER SET ascii COLLATE ascii_bin NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `server`
-  ADD PRIMARY KEY (`id`)
-  ADD UNIQUE KEY `url` (`url`)
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`);
+
+ALTER TABLE `server`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE `project` (
