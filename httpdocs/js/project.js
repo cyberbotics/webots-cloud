0cff00867d3760759f1dba1ecd996335da22dd5a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-10 11:23:34 +0100

    Update project.js

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -53,7 +53,7 @@ export default class Project extends Router {
 `<section class="section">
   <div class="container" style="height:400px">
     <h1 class="title">${data.title}</h1>
-    <webots-animation style="height:80%; display:block;" title="${reference}/animation" playWhenReady=true></webots-animation>
+    <webots-animation style="display:block;" title="${reference}/animation" playWhenReady=true></webots-animation>
   </div>
 </section>`;
     that.setup('animation', [], template.content);
