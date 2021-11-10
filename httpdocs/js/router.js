bd064b8f848c00e6a48d9253888003917b9d1bdb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-10 15:05:50 +0100

    fix

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -94,9 +94,10 @@ export default class Router {
     return promise;
   }
   notFound() {
-    if (window.location.pathname !== '/404.php')
+    if (window.location.pathname !== '/404.php') {
+      console.log('pathname = ' + window.location.pathname);
       window.location.replace('/404.php?pathname=' + window.location.pathname);
-    else {
+    } else {
       console.log('pathname = ' + window.location.pathname);
       const pathname = (window.location.search.startsWith('?pathname=') ? window.location.search.substring(10) : '/404');
       const url = window.location.origin + pathname;
