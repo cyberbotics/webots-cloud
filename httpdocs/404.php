f07ff17d9095abc70d013a19005bd8e0d7f60f69
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-10 17:04:31 +0100

    fix

--- httpdocs/404.php
+++ httpdocs/404.php
@@ -1,4 +1,8 @@
 <?php
-http_response_code(404);
+if ($_SERVER['REQUEST_URI'][0] == 'a')
+  $found = file_exists('animations/' . substr($_SERVER['REQUEST_URI'], 1));
+else
+  $found = false;
+http_response_code($found ? 200 : 404);
 readfile('index.html');
 ?>
