97e9d66e5ea9a979c24e63ccd4c6d2df42b8b95e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-10 17:10:47 +0100

    fix

--- httpdocs/404.php
+++ httpdocs/404.php
@@ -1,9 +1,9 @@
 <?php
-if ($_SERVER['REQUEST_URI'][0] == 'a')
-  $found = file_exists('animations/' . substr($_SERVER['REQUEST_URI'], 1));
+if ($_SERVER['REQUEST_URI'][1] == 'a')
+  $found = file_exists('animations/' . substr($_SERVER['REQUEST_URI'], 2));
 else
   $found = false;
-die("found = " . ($found ? 'true' : 'false') . 'animations/' . substr($_SERVER['REQUEST_URI'], 1));
+die("found = " . ($found ? 'true' : 'false') . ' animations/' . substr($_SERVER['REQUEST_URI'], 2));
 http_response_code($found ? 200 : 404);
 readfile('index.html');
 ?>
