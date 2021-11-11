8650f0aba7eb6893f9cc1825eefa8413009c886a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-11 12:25:51 +0100

    fix

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -47,7 +47,7 @@ export default class Project extends Router {
   animationPage(data) {
     let that = this;
     let template = document.createElement('template');
-    const reference = data.url.substring(data.url.lastIndexOf('/') + 1);
+    const reference = 'storage' + data.url.substring(data.url.lastIndexOf('/'));
     console.log('reference = ' + reference);
     template.innerHTML =
 `<section class="section" style="padding-top:20px">
