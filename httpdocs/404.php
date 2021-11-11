146447ac7a6cea6d288b7df4cb538bdb98a3c233
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-11 12:32:28 +0100

    fix

--- httpdocs/404.php
+++ httpdocs/404.php
@@ -1,7 +1,6 @@
 <?php
-die('storage/' . substr($_SERVER['REQUEST_URI'], 1));
-if ($_SERVER['REQUEST_URI'][1] == 'A')
-  $found = file_exists('storage/' . substr($_SERVER['REQUEST_URI'], 1));
+if (substr($_SERVER['REQUEST_URI'], 5) == '/storage/A')
+  $found = file_exists(substr($_SERVER['REQUEST_URI'], 1));
 else
   $found = false;
 http_response_code($found ? 200 : 404);
