650797996c095057b94e39fe2ea346686df0efd2
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-16 14:58:12 +0100

    fix

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -36,7 +36,7 @@ export default class Router {
       document.body.removeChild(navbar);
     let template = document.createElement('template');
     template.innerHTML =
-`<nav class="navbar is-link is-fixed-top" role="navigation" aria-label="main navigation">
+`<nav class="navbar is-link" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
       <img src="/images/logo-28.png" /> &nbsp; ${this.title}
