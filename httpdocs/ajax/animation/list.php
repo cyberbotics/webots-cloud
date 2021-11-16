6b38543022fd881d0f68ae9a4b6a0441410fb362
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-16 11:10:20 +0100

    fix

--- httpdocs/ajax/animation/list.php
+++ httpdocs/ajax/animation/list.php
@@ -17,7 +17,7 @@
     $url = $mysqli->escape_string($data->url);
     $uri = substr($url, strrpos($url, '/'));
     $id = string_to_mysql_id(substr($uri, 2));  // skipping '/A'
-    $query = "UPDATE animation SET view = view + 1 WHERE id=$id";
+    $query = "UPDATE animation SET viewed = viewed + 1 WHERE id=$id";
     $mysqli->query($query) or error($mysqli->error);
     $query = "SELECT * FROM animation WHERE id=$id";
   } else
