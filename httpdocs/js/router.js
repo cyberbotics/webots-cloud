90eaa683b9dc75166654c203011834fe3d0dd425
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-16 14:47:33 +0100

    fix

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -44,13 +44,13 @@ export default class Router {
   </div>
   <div id="router-navbar" class="navbar-menu">
     <div class="navbar-start">
-    </div>
-    <div class="navbar-end">
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="router-navbar">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
+    </div>
+    <div class="navbar-end">
     </div>
   </div>
 </nav>`;
