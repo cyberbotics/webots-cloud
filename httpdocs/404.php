1a5ceee404eabd2809c9f7f2600262c0e91ff4de
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-17 12:16:49 +0100

    fix

--- httpdocs/404.php
+++ httpdocs/404.php
@@ -1,6 +1,8 @@
 <?php
 if (strlen($_SERVER['REQUEST_URI']) == 8 && $_SERVER['REQUEST_URI'][1] == 'A')
   $found = file_exists('storage' . $_SERVER['REQUEST_URI']);
+elseif ($_SERVER['REQUEST_URI'] == '/settings')
+  $found = true;
 else
   $found = false;
 http_response_code($found ? 200 : 404);
