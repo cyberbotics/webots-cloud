ef25c01b7922dd7613eb41885bd44bf67713c5dd
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-17 15:11:57 +0100

    fix

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -262,7 +262,7 @@ export default class User extends Router {
         });
       });
     }
-    this.load();
+    that.load();
     // account creation: entering the password
     const token = findGetParameter('token');
     if (token) {
