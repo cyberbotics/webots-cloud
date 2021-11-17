6518baa740de896fedbd692d49e1fd42dd75c0ec
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-17 11:34:54 +0100

    fix

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -264,7 +264,7 @@ export default class User extends Router {
           document.querySelector('#user-menu').style.display = 'flex';
           document.querySelector('#log-in').style.display = 'none';
           document.querySelector('#sign-up').style.display = 'none';
-          const md5sum = md5(that.email);
+          const md5sum = md5(that.email.toLowerCase());
           `<script type="text/javascript" src=""></script>`
           let head = document.getElementsByTagName('head')[0];
           let script = document.createElement('script');
