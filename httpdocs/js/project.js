3b5114c53b8cbbe6e19640321fd9e0d4460e4017
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-19 16:15:56 +0100

    fix

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -46,17 +46,21 @@ export default class Project extends User {
     let template = document.createElement('template');
     const reference = 'storage' + data.url.substring(data.url.lastIndexOf('/'));
     const description = data.description.replace('\n', '<br>\n');
+    const view = (Project.webotsView == null) ? `<webots-view id="webots-view" style="height:100%; width:100%; display:block;" data-model="${reference}/model.x3d" data-animation="${reference}/animation.json" data-autoplay=true></webots-view>` : '';
     template.innerHTML =
 `<section class="section" style="padding-top:20px">
-  <div class="container" style="height:540px">
-    <webots-animation style="height:100%; width:100%; display:block;" x3d="${reference}/model.x3d" json="${reference}/animation.json" playWhenReady=true></webots-animation>
-  </div>
+  <div class="container" style="height:540px">${view}</div>
   <div>
     <h1 class="subtitle" style="margin:10px 0">${data.title}</h1>
     ${description}
   </div>
 </section>`;
     that.setup('animation', [], template.content);
+    if (Project.webotsView)
+      Project.webotsView.loadAnimation(`${reference}/model.x3d`, `${reference}/animation.json`);
+    else
+      Project.webotsView = window.querySelector('webots-view');
   }
 }
 Project.current = null;
+Project.webotsView = null;
