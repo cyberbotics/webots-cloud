cbb5d81f1f381e4b1aed6d6175bc6c5675502281
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-19 16:43:55 +0100

    fix

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -56,7 +56,7 @@ export default class Project extends User {
     const view = (!Project.webotsView) ? `<webots-view id="webots-view" style="height:100%; width:100%; display:block;"></webots-view>` : '';
     template.innerHTML =
 `<section class="section" style="padding-top:20px">
-  <div class="container" style="height:540px">${view}</div>
+  <div class="container" style="height:540px" id="webotsViewContainer">${view}</div>
   <div>
     <h1 class="subtitle" style="margin:10px 0">${data.title}</h1>
     ${description}
@@ -65,6 +65,8 @@ export default class Project extends User {
     that.setup('animation', [], template.content);
     if (!Project.webotsView)
       Project.webotsView = document.querySelector('webots-view');
+    else
+      document.querySelector('webotsViewContainer').appendChild(Project.webotsView);
     Project.webotsView.loadAnimation(`${reference}/model.x3d`, `${reference}/animation.json`);
   }
 }
