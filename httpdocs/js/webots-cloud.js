567c87e41d6bfaca7bfe4424e3b04a018ad77dc7
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-24 17:29:52 +0100

    fix

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -336,7 +336,7 @@ document.addEventListener('DOMContentLoaded', function() {
             else {
               modal.close();
               const tr = '<tr class="has-background-warning-light">' + animationRow(data) + '</tr>';
-              let parent = document.querySelector(`section[data-content="${type_name}s"] > div > table > tbody`);
+              let parent = document.querySelector(`section[data-content="${type_name}"] > div > table > tbody`);
               parent.insertAdjacentHTML('beforeend', tr);
               let node = parent.querySelector(`#${type_name}-${data.id}`);
               if (node)
@@ -365,7 +365,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let line = ``;
             for (let i = 0; i < data.length; i++)
               line += '<tr>' + animationRow(data[i]) + '</tr>';
-            let parent = project.content.querySelector(`section[data-content="${type_name}s"] > div > table > tbody`);
+            let parent = project.content.querySelector(`section[data-content="${type_name}"] > div > table > tbody`);
             parent.innerHTML = line;
             for (let i = 0; i < data.length; i++) {
               let node = parent.querySelector(`#${type_name}-${data[i].id}`);
