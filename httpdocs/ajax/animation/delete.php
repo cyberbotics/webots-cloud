d4ad21cf15803be4c8df0e26c6e26c61374f502b
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-25 12:23:58 +0100

    fix

--- httpdocs/ajax/animation/delete.php
+++ httpdocs/ajax/animation/delete.php
@@ -13,10 +13,6 @@ function rrmdir($dir) {  # recursive rmdir, e.g., rm -rf
 header('Content-Type: application/json');
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-if (!isset($data->user))
-  error('Missing user id');
-if (!isset($data->password))
-  error('Missing password');
 if (!isset($data->animation))
   error('Missing animation id');
 $type = isset($data->type) ? strtoupper($data->type[0]) : 'A';
@@ -25,9 +21,9 @@ $mysqli = new mysqli($database_host, $database_username, $database_password, $da
 if ($mysqli->connect_errno)
   error("Can't connect to MySQL database: $mysqli->connect_error");
 $mysqli->set_charset('utf8');
-$user = intval($data->user);
+$user = isset($data->user) ? intval($data->user) : 0;
 $animation = intval($data->animation);
-$password = $mysqli->escape_string($data->password);
+$password = isset($data->password) ? $mysqli->escape_string($data->password) : '';
 $query = "DELETE FROM animation WHERE id=$animation AND (user=0 OR user IN (SELECT id FROM user WHERE id=$user AND password=\"$password\"))";
 $mysqli->query($query) or error($mysqli->error);
 if ($mysqli->affected_rows === 0)
