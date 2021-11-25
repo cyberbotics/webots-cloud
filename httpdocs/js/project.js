a71b87cc6e0ea7fcbad99ebc2880b1e7fbe8bc33
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-25 10:37:14 +0100

    fix

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -22,8 +22,10 @@ export default class Project extends User {
           return response.json();
         })
         .then(function(data) {
-          if (pushHistory)
+          if (pushHistory) {
+            console.log('pushHistory: ' + name + ' ' + url.pathname + url.search + url.hash);
             window.history.pushState(null, name, url.pathname + url.search + url.hash);
+          }
           if (data.length == 0) { // no such animation
             that.notFound();
             resolve();
