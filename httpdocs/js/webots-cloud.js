c0531bb209ddd15ec0b2d4d2ed788626e0d1dc16
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-26 12:49:08 +0100

    fix

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -443,7 +443,7 @@ document.addEventListener('DOMContentLoaded', function() {
               if (node)
                 node.addEventListener('click', function(event) { deleteAnimation(event, type, project); });
             }
-            updatePagination(type_name, 1, 5);
+            updatePagination(type_name, 2, 5);
           }
         });
     }
