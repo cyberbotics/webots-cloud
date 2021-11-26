93e77632664568f1ff5f382ccd42855090de5289
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-11-26 14:41:07 +0100

    fix

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -19,7 +19,9 @@ export default class Router {
             (element.pathname !== document.location.pathname || document.location.hash === element.hash || element.hash === '')) {
           // same-origin navigation: a link within the site (we are skipping linking to the same page with possibly hashtags)
           event.preventDefault(); // prevent the browser from doing the navigation
-          that.load(element.path + element.hash);
+          console.log('load 1 ' + element.pathname + element.hash);
+          console.log('load 2 ' + document.location.path + element.hash);          
+          that.load(element.pathname + element.hash);
           if (element.hash === '')
             window.scrollTo(0, 0);
         }
