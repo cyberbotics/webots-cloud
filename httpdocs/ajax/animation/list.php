f690e91b3891655aead5f0c370c191ef5e079f1f
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-12-22 16:04:01 +0100

    fix

--- httpdocs/ajax/animation/list.php
+++ httpdocs/ajax/animation/list.php
@@ -27,6 +27,7 @@
     $query = "SELECT * FROM animation WHERE id=$id AND $extra_condition";
   } else {  // listing request
     $query = "SELECT id FROM animation WHERE $extra_condition AND viewed = 0 AND user = 0 AND uploaded < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 DAY))";
+    $q2 = $query;
     $result = $mysqli->query($query) or error($mysqli->error);
     require '../../../php/animation.php';
     $counter = 0;
@@ -59,7 +60,7 @@
   $count = $result->fetch_array(MYSQLI_ASSOC);
   $answer = new stdClass;
   $answer->animations = $animations;
-  $answer->counter = $counter;
+  $answer->q2 = $q2;
   $answer->total = intval($count['count']);
   die(json_encode($answer));
  ?>
