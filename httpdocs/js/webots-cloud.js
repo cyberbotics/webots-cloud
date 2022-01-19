40141a88c21ab21a81df66b0cd938895a49e92c0
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-19 10:28:52 +0100

    Update webots-cloud.js

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
       `<footer class="footer">
   <div class="content has-text-centered">
     <p class="has-text-white is-size-7">
-      <a class="has-text-white" target="_blank" href="https://github.com/cyberbotics/webots"><i class="fab fa-github"></i> Webots open-source robot simulator</a>
+      <a class="has-text-white" target="_blank" href="https://github.com/cyberbotics/webots"><i class="fab fa-github is-size-6"></i> open-source robot simulator</a>
       made by <a class="has-text-white" target="_blank" href="https://cyberbotics.com">Cyberbotics</a>
     </p>
   </div>
