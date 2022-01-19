243ac7404812521a3cc10065b9fba6f833b6238a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-19 12:32:37 +0100

    Update router.js

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -2,7 +2,7 @@ export default class Router {
   constructor(title, footer, routes) {
     this.title = title;
     this.content = document.createElement('div');
-    this.content.style = 'height:100%;
+    this.content.style = 'height:100%';
     this.routes = routes;
     const body = document.querySelector('body');
     body.classList.add('has-navbar-fixed-top');
