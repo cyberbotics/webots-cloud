11c2baa4503957fbc20273c8c9da6eb87373b88d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-19 14:43:02 +0100

    test

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -129,6 +129,7 @@ export default class Router {
     this.setup('page not found', [], template.content);
   }
   setup(title, anchors, content, fullpage = false) {
+    console.log('setup');
     document.head.querySelector('#title').innerHTML = this.title + ' - ' + title;
     let menu = '';
     for (let i = 0; i < anchors.length; i++)
