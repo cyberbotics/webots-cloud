a27a5d15d42e989736a653c95e38525865cbfc79
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-25 17:43:28 +0100

    fix

--- httpdocs/ajax/server/pulse.php
+++ httpdocs/ajax/server/pulse.php
@@ -68,7 +68,7 @@
   $query = "INSERT INTO server(url, share) VALUES(\"$url\", $share) ON DUPLICATE KEY UPDATE share=$share";
   $result = $mysqli->query($query) or error($mysqli->error);
   $server_id = $mysqli->insert_id;
-  foreach($repository in $date->repositories) {
+  foreach($repository in $data->repositories) {
     $repo = $mysqli->escape_string($repository);
     $query = "INSERT INTO repository(server, url) VALUES($server_id, \"$repo\")";
     $result = $mysqli->query($query) or error($mysqli->error);
