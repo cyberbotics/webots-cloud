c884b820f421a813fabba012f72f4cd6b6ddd76a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-26 15:34:56 +0100

    fix

--- httpdocs/ajax/server/session.php
+++ httpdocs/ajax/server/session.php
@@ -1,19 +1,28 @@
 <?php
+function error($message) {
+  die("Error: $message");
+}
+function return_url($url) {
+  if (substr($url, 0, 8) === "https://")
+    die('wss://' + substr($url, 8));
+  elseif (substr($url, 0, 7) === "http://")
+    die('ws://' + substr($url, 7));
+  else
+    error("wrong URL protocol in $url");
+}
 require '../../../php/database.php';
 $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
 if ($mysqli->connect_errno)
-  error("Can't connect to MySQL database: $mysqli->connect_error");
+  error("can't connect to MySQL database: $mysqli->connect_error");
 $mysqli->set_charset('utf8');
 $url = $mysqli->escape_string($_GET['url']);
-$query = "SELECT url FROM server WHERE `share` > 0 AND `share` - `load` > 0 AND id IN (SELECT server FROM repository WHERE url=\"$url\") ORDER BY `share` - `load` DESC LIMIT 1";
+$query = "SELECT url FROM server WHERE `load` < 100 AND id IN (SELECT server FROM repository WHERE url=\"$url\") ORDER BY `load` DESC LIMIT 1";
 $result = $mysqli->query($query) or error($mysqli->error);
-if ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-  if (substr($row['url'], 0, 8) === "https://")
-    die('wss://' + substr($row['url'], 8));
-  elseif (substr($row['url'], 0, 7) === "http://")
-    die('ws://' + substr($row['url'], 7));
-  else
-    die("Error: wrong URL protocol in $row");
-}
-die('Error: no simulation server available at the moment.');
+if ($row = $result->fetch_array(MYSQLI_ASSOC))
+  return_url($row['url']);
+$query = "SELECT url FROM server WHERE `share` > 0 AND `share` - `load` > 0 ORDER BY `share` - `load` DESC LIMIT 1";
+$result = $mysqli->query($query) or error($mysqli->error);
+if ($row = $result->fetch_array(MYSQLI_ASSOC))
+  return_url($row['url']);
+error('no simulation server available at the moment.');
 ?>
