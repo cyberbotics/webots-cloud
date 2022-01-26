9405714a2ba4fb1fceede72e14a8c463c7ca2add
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-01-26 15:53:32 +0100

    fix

--- httpdocs/ajax/server/session.php
+++ httpdocs/ajax/server/session.php
@@ -16,14 +16,16 @@ if ($mysqli->connect_errno)
   error("can't connect to MySQL database: $mysqli->connect_error");
 $mysqli->set_charset('utf8');
 $url = $mysqli->escape_string($_GET['url']);
+# search for a dedicated server first
 $query = "SELECT url FROM server WHERE `load` < 100 AND id IN (SELECT server FROM repository WHERE \"$url%\" LIKE CONCAT(url, '%')) ORDER BY `load` DESC LIMIT 1";
-print($query);
 $result = $mysqli->query($query) or error($mysqli->error);
 if ($row = $result->fetch_array(MYSQLI_ASSOC))
   return_url($row['url']);
+# no available dedicated server found, reverting to a public server
 $query = "SELECT url FROM server WHERE `share` > 0 AND `share` - `load` > 0 ORDER BY `share` - `load` DESC LIMIT 1";
 $result = $mysqli->query($query) or error($mysqli->error);
 if ($row = $result->fetch_array(MYSQLI_ASSOC))
-  return_url($row['url'] . '2');
+  return_url($row['url']);
+# no public server currently available
 error('no simulation server available at the moment.');
 ?>
