468de463577178a7b0b2d3400c08be052a6d68e5
ThomasOliverKimble <tokimble@gmail.com>
2022-03-14 18:17:13 +0100

    sync

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -196,6 +196,7 @@ document.addEventListener('DOMContentLoaded', function() {
         `<td class="has-text-centered">${competitors}</td>` +
         `<td class="synchronizable">
           <span class="has-text-right is-size-7" title="Last synchronization with GitHub">${updated}</span>
+          <i class="fas fa-sync fa-m synchronizable-icon" title="Re-synchronize now"></i>
         </td>`;
       return row;
     }
