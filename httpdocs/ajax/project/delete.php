91f1cc3aa3ef90e2fc9efe0d93235b6ef607cd45
ThomasOliverKimble <tokimble@gmail.com>
2022-03-15 17:40:41 +0100

    testing

--- httpdocs/ajax/project/delete.php
+++ httpdocs/ajax/project/delete.php
@@ -5,6 +5,7 @@ function error($message) {
 header('Content-Type: application/json');
 $json = file_get_contents('php://input');
 $data = json_decode($json);
+echo "Data: $data";
 if (!isset($data->simulation))
   error('Missing simulation id');
 require '../../../php/simulation.php';
