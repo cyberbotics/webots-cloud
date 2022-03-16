f687d30dd9db2e40321831f4850f11c22a3382c2
ThomasOliverKimble <tokimble@gmail.com>
2022-03-16 15:40:49 +0100

    testing

--- httpdocs/ajax/animation/delete.php
+++ httpdocs/ajax/animation/delete.php
@@ -5,7 +5,6 @@ function error($message) {
 header('Content-Type: application/json');
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-echo "Going here with variable $data->animation ...";
 if (!isset($data->animation))
   error('Missing animation id');
 $type = isset($data->type) ? strtoupper($data->type[0]) : 'A';
