8af4e2152bd66518c9f06bdc1368248136cba542
ThomasOliverKimble <tokimble@gmail.com>
2022-03-16 16:37:49 +0100

    testing

--- httpdocs/ajax/project/delete.php
+++ httpdocs/ajax/project/delete.php
@@ -20,7 +20,7 @@ $mysqli->set_charset('utf8');
 $user = isset($data->user) ? intval($data->user) : 0;
 $simulation = intval($data->simulation);
 $password = isset($data->password) ? $mysqli->escape_string($data->password) : '';
-$query = "DELETE FROM simulation WHERE id=$simulation AND (user=0 OR user IN (SELECT id FROM user WHERE id=$user AND password=\"$password\"))";
+$query = "DELETE FROM animation WHERE id=$animation AND (user=0 OR user IN (SELECT id FROM user WHERE id=$user AND password=\"$password\"))";
 $mysqli->query($query) or error($mysqli->error);
 
 error("I really tried to delete simulation $data->simulation...");
