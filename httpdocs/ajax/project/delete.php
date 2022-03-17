f8cdfda1177ed27b932fc88929a791775e991e7f
ThomasOliverKimble <tokimble@gmail.com>
2022-03-17 15:40:29 +0100

    minor mods

--- httpdocs/ajax/project/delete.php
+++ httpdocs/ajax/project/delete.php
@@ -2,15 +2,12 @@
 function error($message) {
   die("{\"error\":\"$message\"}");
 }
-
 header('Content-Type: application/json');
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-
 if (!isset($data->id))
   error('Missing simulation id');
 $id = intval($data->id);
-
 require '../../../php/database.php';
 $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
 if ($mysqli->connect_errno)
