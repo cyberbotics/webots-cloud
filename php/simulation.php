f7e5e128445512e231769bbc6fc3a5669670c0b3
ThomasOliverKimble <tokimble@gmail.com>
2022-03-23 11:02:57 +0100

    yaml testing

--- php/simulation.php
+++ php/simulation.php
@@ -49,9 +49,9 @@ function simulation_check_yaml($check_url) {
 
   $line = strtok($yaml_content, "\r\n");
   while ($line !== false) {
-    return "hello";
     if (substr($line, 0, 5) === 'uses:')
       $docker = trim(substr($line, 6), " ");
+      return "hello";
     elseif (substr($line, 0, 5) === 'type:')
       $type = trim(substr($line, 6), " ");
     elseif (substr($line, 0, 11) === 'simulation:') {
