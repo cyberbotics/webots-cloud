bb92a58644289f6b6a67b1cca2ae74ff3401f140
ThomasOliverKimble <tokimble@gmail.com>
2022-03-23 11:49:39 +0100

    yaml testing

--- php/simulation.php
+++ php/simulation.php
@@ -48,8 +48,9 @@ function simulation_check_yaml($check_url) {
   $worlds_simulation = array();
   $worlds_animation = array();
 
+  return "Hello";
   $yaml_content = implode("\n", array_filter(explode("\n", $yaml_content)));
-
+  return "Hello here";
   $line = strtok($yaml_content, "\r\n");
   while ($line !== false) {
     elseif (substr($line, 0, 5) === 'uses:')
