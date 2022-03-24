b8d3252ff3c51732046cdf7df24ae3f41438d3c0
ThomasOliverKimble <tokimble@gmail.com>
2022-03-24 11:59:13 +0100

    yaml testing

--- httpdocs/ajax/project/create.php
+++ httpdocs/ajax/project/create.php
@@ -38,7 +38,7 @@ if ($world_content === false) {
 $check_yaml = simulation_check_yaml($check_url);
 if (!is_array($check_yaml))
   error($check_yaml);
-list($docker, $type, $publish, $worlds, $benchmark, $competition) = $check_yaml;
+list($docker, $type, $publish, $world_temp, $benchmark, $competition, $simulation_worlds, $animation_worlds, $animation_durations) = $check_yaml;
 if ($publish === 'false') {
   $query = "DELETE FROM project WHERE id=$id";
   $mysqli->query($query) or error($mysqli->error);
