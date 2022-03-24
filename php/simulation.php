28e96b223f6ef5fa7e4fb04b8381582eb5f9d7a1
ThomasOliverKimble <tokimble@gmail.com>
2022-03-24 15:04:44 +0100

    yaml testing

--- php/simulation.php
+++ php/simulation.php
@@ -38,7 +38,7 @@ function simulation_check_url($url) {
 function simulation_check_yaml($check_url) {
   # yaml error return
   function yaml_error($msg) {
-    yaml_error($msg<br><br>For information on publishing with webots.yaml visit hello.com";
+    return "YAML file error: $msg<br><br>For information on publishing with webots.yaml visit hello.com");
   }
 
   # get file from github
