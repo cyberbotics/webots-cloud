2a8ac7c5c3664396ba3e42ad8ec88f60097d0c43
ThomasOliverKimble <tokimble@gmail.com>
2022-03-25 14:44:12 +0100

    modified yaml errors during parsing

--- php/simulation.php
+++ php/simulation.php
@@ -93,12 +93,12 @@ function simulation_check_yaml($check_url) {
   elseif ($type === 'demo') {
     if(count($worlds) == 0)
       array_push($worlds, $world_url);
-  }/* elseif ($type === 'benchmark' || $type === 'competition') {
+  }elseif ($type === 'benchmark' || $type === 'competition') {
     if (count($worlds) == 0))
       array_push($worlds, $world_url);
     elseif (count($worlds) > 0)
       return yaml_error("type $type does not require worlds to be defined.");
-  } elseif ($type === 'competitor') {
+  } /* elseif ($type === 'competitor') {
     if ($benchmark !== '' && $competition !== '')
       return yaml_error("competitor type only requires one scenario (benchmark or competition)");
     elseif ($benchmark === '' && $competition === '')
