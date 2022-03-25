548900d2ebbcf5eaf334076679cc9a88f55b5502
ThomasOliverKimble <tokimble@gmail.com>
2022-03-25 15:54:03 +0100

    session server on testing

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -78,12 +78,13 @@ export default class Project extends User {
     else
       Project.webotsView.loadScene(`${reference}/scene.x3d`);
   }
-  runPage() {
+  runPage(worlds) {
     this.setupWebotsView('run');
     const url = this.findGetParameter('url');
     const mode = this.findGetParameter('mode');
     Project.webotsView.connect('https://testing.webots.cloud/ajax/server/session.php?url=' + url, mode);
     Project.webotsView.showQuit = false;
+    Project.webotsView.worlds = worlds;
   }
 }
 Project.current = null;
