373ede90ffab300977c4eef1fb4bed9584d43703
ThomasOliverKimble <tokimble@gmail.com>
2022-03-25 14:48:37 +0100

    modified yaml errors during parsing

--- php/simulation.php
+++ php/simulation.php
@@ -105,7 +105,6 @@ function simulation_check_yaml($check_url) {
       return yaml_error("competitor type requires a scenario (benchmark or competition)");
   } else
     return yaml_error("type not defined.");
-  */
 
   # return array with YAML file info
   return array($type, $publish, $worlds, $benchmark, $competition);
