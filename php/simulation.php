95e633006514161b4bf157441546287173af77b2
ThomasOliverKimble <tokimble@gmail.com>
2022-03-30 16:14:04 +0200

    yaml testing

--- php/simulation.php
+++ php/simulation.php
@@ -57,7 +57,6 @@ function simulation_check_yaml($check_url) {
   $type = '';
   $benchmark = '';
   $competition = '';
-  $init_end = false;
 
   # delete empty lines
   $yaml_content = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $yaml_content);
@@ -72,14 +71,6 @@ function simulation_check_yaml($check_url) {
       $benchmark = trim(substr($line, 10), " ");
     elseif (substr($line, 0, 12) === 'competition:')
       $competition = trim(substr($line, 12), " ");
-    elseif (substr($line, 0, 5) === 'init:') {
-      $line = strtok("\r\n");
-      $init_end = true;
-    }
-    if $init_end = true
-      $init_end = false;
-    else
-      $line = strtok("\r\n");
   }
 
   # check if configuration makes sense
