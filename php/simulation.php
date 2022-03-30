b76a8a332b01f257bc11a7e76b98e07b1016e35f
ThomasOliverKimble <tokimble@gmail.com>
2022-03-30 15:35:31 +0200

    yaml testing

--- php/simulation.php
+++ php/simulation.php
@@ -58,10 +58,10 @@ function simulation_check_yaml($check_url) {
   $benchmark = '';
   $competition = '';
   $worlds = array();
-  $world_list_end = false;
 
   # delete empty lines
   $yaml_content = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $yaml_content);
+
   # parse yaml file
   $line = strtok($yaml_content, "\r\n");
   while ($line !== false) {
@@ -73,39 +73,18 @@ function simulation_check_yaml($check_url) {
       $benchmark = trim(substr($line, 10), " ");
     elseif (substr($line, 0, 12) === 'competition:')
       $competition = trim(substr($line, 12), " ");
-    elseif (substr($line, 0, 7) === 'worlds:') {
-      $line = strtok("\r\n");
-      while (substr($line, 0, 9) === '  - file:') {
-        array_push($worlds, trim(substr($line, 9), " "));
-        $line = strtok("\r\n");
-      }
-      $world_list_end = true;
-    }
-    if ($world_list_end === true)
-      $world_list_end = false;
-    else
-      $line = strtok("\r\n");
+    $line = strtok("\r\n");
   }
 
   # check if configuration makes sense
   if ($publish === 'false')
-    return yaml_error("Project publish failed. Make sure to set 'publish: true' in webots.yaml");
-  elseif ($type === 'demo') {
-    if (count($worlds) === 0)
-      array_push($worlds, $world_from_url);
-    elseif (!in_array($world_from_url, $worlds))
-      return yaml_error("$world_from_url not in webots.yaml world list.");
-  } elseif ($type === 'benchmark' || $type === 'competition') {
-    if (count($worlds) === 0)
-      array_push($worlds, $world_from_url);
-    else
-      return yaml_error("type $type does not require worlds to be defined.");
+    return yaml_error("Project publishing failed. Make sure to set 'publish: true' in webots.yaml");
   } elseif ($type === 'competitor') {
     if ($benchmark !== '' && $competition !== '')
       return yaml_error("competitor type only requires one scenario (benchmark or competition)");
     elseif ($benchmark === '' && $competition === '')
       return yaml_error("competitor type requires a scenario (benchmark or competition)");
-  } else
+  } elseif ($type === '')
     return yaml_error("type not defined.");
 
   # return array with YAML file info
