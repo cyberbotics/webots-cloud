58a5b056c5f130148b8a7bb832d7d69f925e49ae
ThomasOliverKimble <tokimble@gmail.com>
2022-03-31 12:06:57 +0200

    yaml init parsing testing

--- php/simulation.php
+++ php/simulation.php
@@ -77,6 +77,7 @@ function simulation_check_yaml($check_url) {
         $line = strtok("\\\r\n");
         $line = strtok("\\\r\n");
         $line = strtok("\\\r\n");
+        $line = strtok("\\\r\n");
         return "Line read: $line";
         while (true) {
           if (substr($line, 0, 2) === '  ') {
