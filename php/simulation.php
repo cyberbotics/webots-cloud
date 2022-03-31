dc64c85f1fd4a0e7c3e3648da4cea16ff800cafd
ThomasOliverKimble <tokimble@gmail.com>
2022-03-31 11:49:26 +0200

    yaml init parsing testing

--- php/simulation.php
+++ php/simulation.php
@@ -74,7 +74,7 @@ function simulation_check_yaml($check_url) {
       $competition = trim(substr($line, 12), " ");
     elseif (substr($line, 0, 5) === 'init:') {
       $line = strtok("\r\n");
-      $line = strtok("\\");
+      $line = strtok("\r\n");
       $temp = gettype($line);
       return "Type of line: $temp";
       if (trim(substr($line, 5), " ") === '|') {
