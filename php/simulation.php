8dbc08efd6e7095dc3d0a69dbb45452cff7c9bc0
ThomasOliverKimble <tokimble@gmail.com>
2022-03-31 15:29:18 +0200

    testing

--- php/simulation.php
+++ php/simulation.php
@@ -20,16 +20,14 @@ function simulation_check_url($url) {
   if (!preg_match('/^[a-z\d_.-]{0,100}$/i', $version))
     return 'Wrong GitHub tag or branch name';
   $folder = implode('/', array_slice($exploded, 4, $count - 6));
-  if (!preg_match('/^[a-z\d_.-\/]{1,100}$/i', $folder)
+  if (!preg_match('/^[a-z\d_.-\/]{1,100}$/i', $folder))
     return "oh dear 1...";
-  if (!preg_match('/^[a-z\d_.-\/]{1,100}$/i', $folder)
-    return "oh dear 2...";
   if (substr($folder, 0, 1) === '/')
-    return "oh dear 3...";
+    return "oh dear 2...";
   if (strstr($folder, '//'))
-    return "oh dear 4...";
+    return "oh dear 3...";
   if (substr($folder, -1) === '/')
-    return "oh dear 5...";
+    return "oh dear 4...";
   if ($folder !=='' and
       (!preg_match('/^[a-z\d_.-\/]{1,100}$/i', $folder)  # no fancy folder name
       or substr($folder, 0, 1) === '/'                  # doesn't start with slash
