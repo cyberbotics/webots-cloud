47e37dd70fe647c1e3f8f1e90b9a500cd3e03006
ThomasOliverKimble <tokimble@gmail.com>
2022-04-01 12:13:01 +0200

    minor mods

--- php/database.php
+++
@@ -1,6 +0,0 @@
-<?php  # Fill in your database password and copy this file to database.php
-$database_host = 'qi2rs.myd.infomaniak.com';
-$database_name = 'qi2rs_webots';
-$database_username = 'qi2rs_webots';
-$database_password = 'tarFil2_be';  # insert your password here
-?>
