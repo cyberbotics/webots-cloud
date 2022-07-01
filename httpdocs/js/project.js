f3ce47ace3562e0dd3de95e4c051aabe79e5110f
ThomasOliverKimble <tokimble@gmail.com>
2022-07-01 10:08:49 +0200

    terms of use testing

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -5,6 +5,19 @@ export default class Project extends User {
   constructor(title, footer, routes) {
     super(title, footer, routes);
     this.load();
+    this.routes.push({url: '/terms-of-use', setup: termsOfUsePage});
+    let that = this;
+    function termsOfUsePage() {
+      const template = document.createElement('template');
+      template.innerHTML =
+        `<section class="section">
+          <div class="container">
+            <h1 class="title pb-3"><i class="fas fa-pencil-square-o></i> Terms of Use</h1>
+            <h2 class="subtitle pt-3">Cyberbotics Ltd.</h2>
+          </div>
+        </section>`
+      that.setup('terms-of-use', [], template.content);
+    }
   }
   static run(title, footer, routes) {
     Project.current = new Project(title, footer, routes);
