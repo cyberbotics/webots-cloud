65aceec23c9a541a5f377cf2a7935d241d5ef71f
ThomasOliverKimble <tokimble@gmail.com>
2022-07-04 17:36:24 +0200

    update layout

--- httpdocs/js/termsAndPrivacy.js
+++ httpdocs/js/termsAndPrivacy.js
@@ -7,7 +7,7 @@ export default class TermsAndPrivacy {
       template.innerHTML =
         `<section class="section">
           <div class="container content">
-            <h1 class="title pb-5"><i class="fa-solid fa-sm fa-shield-halved"></i>Terms of Use</h1>
+            <h1 class="title pb-4"><i class="fa-solid fa-sm fa-shield-halved"></i>  Terms of Use</h1>
             <ul>
               <li>Effective: 4 July 2022</li>
               <li>Last updated: 4 July 2022</li>
@@ -16,11 +16,11 @@ export default class TermsAndPrivacy {
               Cyberbotics Sàrl, headquartered at EPFL Innovation Park, Building C, 1015 Lausanne, Switzerland, registered 
               with the Swiss UID CHE-104.504.228 (hereinafter “Cyberbotics” or “we”). These Terms of Service are a legal 
               agreement between you and Cyberbotics. They apply to your use of our services.</p>
-            <h2 class="subtitle pt-4">Creating an Account</h2>
+            <h2 class="subtitle pt-4">Creating an account</h2>
             <p>In order to use our services, you may need to create an account with an e-mail address and a password. You are 
               responsible for keeping your password secure. Our <a target="_blank" href="https://webots.cloud/privacy-policy">
               Privacy Policy</a> informs you on what information we collect and how we use it.</p>
-            <h2 class="subtitle pt-4">Our Services</h2>
+            <h2 class="subtitle pt-4">Our services</h2>
             <p>We provide you with the following services:</p>
             <ul>
               <li>Upload any Webots world file (<a target="_blank" href="https://webots.cloud/scene">scene</a>) and view it 
@@ -33,12 +33,12 @@ export default class TermsAndPrivacy {
               <li>Register your own server (<a target="_blank" href="https://webots.cloud/server">server</a>) to run the 
                 simulations.</Li>
             </ul>
-            <h2 class="subtitle pt-4">Your Content</h2>
+            <h2 class="subtitle pt-4">Your content</h2>
             <p>Your content includes the scenes, animations and simulations you provide. You are the owner of your content. 
               By using our services, you agree that your scenes, animations and simulations visualizations are made publicly 
               available under the terms of Creative Common license. We reserve the right to remove your content for any 
               reason.</p>
-            <h2 class="subtitle pt-4">Service Availability</h2>
+            <h2 class="subtitle pt-4">Service availability</h2>
             <p>We do reasonable efforts to keep webots.cloud operational but we cannot guarantee it will be available at all 
               times. We may for example need to perform maintenance or experience software or hardware problems.</p>
             <h2 class="subtitle pt-4">Applicable Law</h2>
@@ -55,7 +55,7 @@ export default class TermsAndPrivacy {
       template.innerHTML =
         `<section class="section">
           <div class="container content">
-            <h1 class="title pb-5"><i class="fa-solid fa-sm fa-lock-keyhole"></i>Privacy Policy</h1>
+            <h1 class="title pb-4"><i class="fa-solid fa-sm fa-lock-keyhole"></i>  Privacy Policy</h1>
             <ul>
               <li>Effective: 4 July 2022</li>
               <li>Last updated: 4 July 2022</li>
