f8b8979022512a09bf1cbd346dc1ca55fd3b4add
ThomasOliverKimble <tokimble@gmail.com>
2022-07-04 15:27:09 +0200

    add terms page

--- httpdocs/js/terms.js
+++ httpdocs/js/terms.js
@@ -3,15 +3,20 @@ export default class Terms {
     routes.push({url: '/terms-of-use', setup: termsOfUsePage});
     let that = this;
     function termsOfUsePage() {
-      const template = document.createElement('template');
-      template.innerHTML =
+      const content = document.createElement('template');
+      content.innerHTML =
         `<section class="section">
           <div class="container">
             <h1 class="title pb-3"><i class="fa-solid fa-sm fa-shield-halved"></i> Terms of Use</h1>
             <h2 class="subtitle pt-3">Cyberbotics Ltd.</h2>
           </div>
         </section>`
-      that.setup('terms-of-use', [], template.content);
+      document.head.querySelector('#title').innerHTML = 'terms-of-use';
+      if (content.childNodes) {
+        content.childNodes.forEach(function(item) {
+          that.content.appendChild(item);
+        });
+      }
     }
   }
 }
