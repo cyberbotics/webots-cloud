e1b63e7a09202af5c0ac2f2f380ba70599a46d78
ThomasOliverKimble <tokimble@gmail.com>
2022-07-04 15:39:17 +0200

    test paragraph

--- httpdocs/js/terms.js
+++ httpdocs/js/terms.js
@@ -8,7 +8,13 @@ export default class Terms {
         `<section class="section">
           <div class="container">
             <h1 class="title pb-3"><i class="fa-solid fa-sm fa-shield-halved"></i> Terms of Use</h1>
-            <h2 class="subtitle pt-3">Cyberbotics Ltd.</h2>
+            <h2 class="subtitle pt-3 pb-3">Introduction</h2>
+            <p>
+              The website https://webots.cloud (“webots.cloud”) is created, developed and operated by Cyberbotics Sàrl, 
+              headquartered at EPFL Innovation Park, Building C, 1015 Lausanne, Switzerland, registered with the Swiss UID 
+              CHE-104.504.228 (hereinafter “Cyberbotics” or “we”). </br> These Terms of Service are a legal agreement between 
+              you and Cyberbotics. They apply to your use of our services.
+            </p>
           </div>
         </section>`
       project.setup('terms-of-use', [], template.content);
