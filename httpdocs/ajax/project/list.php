f009a7a6df759394edfcbf33cac6e481636f3b6f
ThomasOliverKimble <tokimble@gmail.com>
2022-07-05 13:59:56 +0200

    testing

--- httpdocs/ajax/project/list.php
+++ httpdocs/ajax/project/list.php
@@ -12,7 +12,8 @@
   $mysqli->set_charset('utf8');
   if (isset($data->url)) {
     $url = $data->url;
-    $query = "UPDATE project SET viewed = rand(20,230) WHERE url LIKE \"$url\"";
+    $views = mt_rand(20,230);
+    $query = "UPDATE project SET viewed = $views WHERE url LIKE \"$url\"";
     $mysqli->query($query) or error($mysqli->error);
     die('{"status":"updated"}');
   }
