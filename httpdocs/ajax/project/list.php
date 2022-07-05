8b3d01922ec5163bb99a52b71bbf74cc97b271ad
ThomasOliverKimble <tokimble@gmail.com>
2022-07-05 11:11:07 +0200

    testing

--- httpdocs/ajax/project/list.php
+++ httpdocs/ajax/project/list.php
@@ -11,6 +11,14 @@
     error("Can't connect to MySQL database: $mysqli->connect_error");
   $mysqli->set_charset('utf8');
   if (isset($data->url)) {
+    $query1 = "DESCRIBE project";
+    $result1 = $mysqli->query($query1) or error($mysqli->error);
+    $fields = "";
+    while($row = $result1->fetch_array(MYSQLI_ASSOC)) {
+      $fields .= $row['Field'];
+      $fields .= " ";
+    }
+    error("Fields: $fields");
     $url = $data->url;
     $query = "UPDATE project SET viewed = viewed + 1 WHERE url LIKE $url";
     $mysqli->query($query) or error($mysqli->error);
