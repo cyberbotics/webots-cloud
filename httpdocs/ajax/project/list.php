fce33351fc21571ea2404299375beabb68b14f51
ThomasOliverKimble <tokimble@gmail.com>
2022-07-05 11:36:12 +0200

    testing

--- httpdocs/ajax/project/list.php
+++ httpdocs/ajax/project/list.php
@@ -11,7 +11,7 @@
     error("Can't connect to MySQL database: $mysqli->connect_error");
   $mysqli->set_charset('utf8');
   if (isset($data->url)) {
-    $query1 = "UPDATE project SET viewed=10500";
+    $query1 = "UPDATE project SET viewed=0";
     $result1 = $mysqli->query($query1) or error($mysqli->error);
 
     $query1 = "SELECT DISTINCT viewed FROM project";
