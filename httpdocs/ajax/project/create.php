be43783486a0f29956247200bbd75d94bbf58163
ThomasOliverKimble <tokimble@gmail.com>
2022-07-05 13:55:44 +0200

    testing

--- httpdocs/ajax/project/create.php
+++ httpdocs/ajax/project/create.php
@@ -85,7 +85,7 @@ $competitors = 0;
 $query = "SELECT viewed FROM project WHERE url=\"$url\" AND id=$id";
 $result = $mysqli->query($query) or error($mysqli->error);
 if ($result) {
-  $row = $result->fetch_assoc(MYSQLI_ASSOC);
+  $row = $result->fetch_array(MYSQLI_ASSOC);
   $viewed = $row['viewed'];
 } else
   $viewed = 0;
