9e39c30e39fb46f59f41c498a38b7ab4f277aef7
ThomasOliverKimble <tokimble@gmail.com>
2022-07-06 16:02:24 +0200

    search testing

--- httpdocs/ajax/animation/list.php
+++ httpdocs/ajax/animation/list.php
@@ -41,6 +41,7 @@
     if (isset($data->search)) {
       $searchString = $mysqli->escape_string($data->search);
       $extra_condition += " AND title LIKE '%$searchString%'";
+      error($extra_condition);
     }
     $parameter = $data->sortBy = isset($data->sortBy) ? $data->sortBy : "viewed";
     $order = $parameter == "title" || $parameter == "version" ? "ASC" : "DESC";
