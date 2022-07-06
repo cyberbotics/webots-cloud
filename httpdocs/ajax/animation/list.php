8a8c051b35134094b373898dffd6948a19ee9c9f
ThomasOliverKimble <tokimble@gmail.com>
2022-07-06 16:01:58 +0200

    search testing

--- httpdocs/ajax/animation/list.php
+++ httpdocs/ajax/animation/list.php
@@ -38,8 +38,10 @@
       $mysqli->query("DELETE FROM animation WHERE id=$id");
       delete_animation($type, $id);
     }
-    if (isset($data->search))
-      $extra_condition += " AND title LIKE '%$mysqli->escape_string($data->search)%'";
+    if (isset($data->search)) {
+      $searchString = $mysqli->escape_string($data->search);
+      $extra_condition += " AND title LIKE '%$searchString%'";
+    }
     $parameter = $data->sortBy = isset($data->sortBy) ? $data->sortBy : "viewed";
     $order = $parameter == "title" || $parameter == "version" ? "ASC" : "DESC";
     $query = "SELECT * FROM animation WHERE $extra_condition AND uploading = 0 ORDER BY $parameter $order, id ASC LIMIT $limit OFFSET $offset";
