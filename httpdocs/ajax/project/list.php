67d13fedcddba7659e082e6e8ea79e21fe1aab5c
ThomasOliverKimble <tokimble@gmail.com>
2022-07-06 17:17:24 +0200

    simulation search/sort testing

--- httpdocs/ajax/project/list.php
+++ httpdocs/ajax/project/list.php
@@ -21,9 +21,8 @@
   $order = $parameter == "title" || $parameter == "version" ? "ASC" : "DESC";
   if (isset($data->search))
     $searchString = $mysqli->escape_string($data->search);
-  error("gone here");
   $extra_condition = $searchString ? "WHERE title LIKE '%$searchString%'" : "";
-  error("extra condition: " + $extra_condition);
+  error("extra condition: $extra_condition");
   $offset = isset($data->offset) ? intval($data->offset) : 0;
   $limit = isset($data->limit) ? intval($data->limit) : 10;
   $query = "SELECT * FROM project $extra_condition ORDER BY $parameter DESC LIMIT $limit OFFSET $offset";
