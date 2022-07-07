ed1040ced886bba0036b6391c0c73166816ecd12
ThomasOliverKimble <tokimble@gmail.com>
2022-07-07 14:44:39 +0200

    testing

--- httpdocs/ajax/project/list.php
+++ httpdocs/ajax/project/list.php
@@ -30,6 +30,8 @@
   $result = $mysqli->query($query) or error($mysqli->error);
   $projects = array();
   while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $tempUrl = $row['url'];
+    error("URL: $tempUrl");
     settype($row['id'], 'integer');
     settype($row['viewed'], 'integer');
     settype($row['stars'], 'integer');
