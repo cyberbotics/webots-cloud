b47b5346154e00e4b8ee2786f1507c0da7b9f63d
ThomasOliverKimble <tokimble@gmail.com>
2022-07-08 17:07:04 +0200

    search and sort testing

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function searchAndSortTable(type, isSearch) {
       setSearches(type, document.getElementById(type + '-search-input').value);
       setSorts(type, document.getElementById(type + '-sort-select').value);
-      console.log(getSearch(type));
+      console.log(document.getElementById(type + '-sort-select').value));
       let url = new URL(document.location.origin + document.location.pathname);
       if (getPage(type) !== 1 && !isSearch)
         url.searchParams.append('p', getPage(type));
