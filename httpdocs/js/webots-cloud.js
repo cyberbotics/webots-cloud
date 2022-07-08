9fab88281ef44ee54ef6f0aa515aa3cf8781623d
ThomasOliverKimble <tokimble@gmail.com>
2022-07-08 15:56:15 +0200

    search and sort testing

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -163,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function pushHistoryForSearch() {
       if (window.history.state !== 'search')
-        console.log(url.pathname.toString() + '/' + activeTab);
+        console.log(document.location + activeTab);
         //window.history.pushState('search', '', (url.pathname + url.search).toString());
     }
 
