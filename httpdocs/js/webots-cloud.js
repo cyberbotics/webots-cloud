57237f3b9fba206bc1f2fcd7792d4f842cc2d0da
ThomasOliverKimble <tokimble@gmail.com>
2022-07-11 09:04:50 +0200

    search and sort testing

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -162,13 +162,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function searchAndSortTable(type, isSearch) {
       if (isSearch) {
-        const searchString = document.getElementById(type + '-search-input');
+        const searchString = document.getElementById(type + '-search-input').value;
         setSearches(type, searchString);
         const searchIcon = document.getElementById(type + '-search-icon');
-        if (searchIcon.classList.contains('fa-search') && searchString.value.length > 0) {
+        if (searchIcon.classList.contains('fa-search') && searchString.length > 0) {
           searchIcon.classList.remove('fa-search');
           searchIcon.classList.add('fa-xmark');
-        } else if (searchIcon.classList.contains('fa-xmark') && searchString.value.length === 0) {
+        } else if (searchIcon.classList.contains('fa-xmark') && searchString.length === 0) {
           searchIcon.classList.add('fa-search');
           searchIcon.classList.remove('fa-xmark');
         }
