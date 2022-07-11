5a3cd23939ca45a72cb1ea514af69ff7b1faed59
ThomasOliverKimble <tokimble@gmail.com>
2022-07-11 10:52:45 +0200

    search and sort testing

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -192,8 +192,8 @@ document.addEventListener('DOMContentLoaded', function() {
       const searchIcon = document.getElementById(type + '-search-icon');
       if (searchIcon.classList.contains('fa-xmark') && getSearch(type).length > 0) {
         document.getElementById(type + '-search-input').value = '';
-        searchIcon.classList.add('fa-search');
         searchIcon.classList.remove('fa-xmark');
+        searchIcon.classList.add('fa-search');
         searchAndSortTable(type, true);
       }
     }
