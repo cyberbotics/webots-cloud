864d3aafd148917d7be225aad2f6f784bac08832
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:54:42 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -85,26 +85,23 @@ export default class User extends Router {
 
       template.innerHTML =
       `<section class="section">
-          <div class="tile is-vertical is-parent is-8">
-            <div class="tile is-child">
-              <article class="tile is-child notification is-warning box">
-                <p class="title">My Projects</p>
-                <div class="content">
-                  ${projectsTable}
-                </div>
-              </article>
+        <div class="tile is-ancestor">
+          <div class="tile is-4 is-vertical is-parent">
+            <div class="tile is-child box">
+              <p class="title">My Projects</p>
+              <div class="content">
+                ${projectsTable}
+              </div>
             </div>
           </div>
-          <div class="tile is-vertical is-parent">
-            <div class="tile is-child is-success box">
+          <div class="tile is-parent">
+            <div class="tile is-child box">
               <p class="title">One</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
-                Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
             </div>
-            <div class="tile is-child is-primary box">
+            <div class="tile is-child box">
               <p class="title">Two</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
-                Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
             </div>
           </div>
         </div>`;
