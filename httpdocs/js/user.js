cced1184319b5e11ab4e246df577ea68ed704bf0
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:26:10 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -23,12 +23,58 @@ export default class User extends Router {
       // we need to be logged in to view this page
       if (!that.password || !that.email)
        return false;
-     const emailBeginning = that.email.substring(0, that.email.indexOf("@"));
-     const template = document.createElement('template');
-     const md5sum = md5(that.email.toLowerCase());
-     const hostname = document.location.hostname;
-     const name = (typeof displayName === 'undefined') ? emailBeginning : displayName;
-     template.innerHTML =
+      const emailBeginning = that.email.substring(0, that.email.indexOf("@"));
+      const template = document.createElement('template');
+      const md5sum = md5(that.email.toLowerCase());
+      const hostname = document.location.hostname;
+      const name = (typeof displayName === 'undefined') ? emailBeginning : displayName;
+      template.innerHTML =
+      `<div class="tile is-ancestor">
+        <div class="tile is-vertical is-8">
+          <div class="tile">
+            <div class="tile is-parent is-vertical">
+              <article class="tile is-child notification is-primary">
+                <p class="title">Vertical...</p>
+                <p class="subtitle">Top tile</p>
+              </article>
+              <article class="tile is-child notification is-warning">
+                <p class="title">...tiles</p>
+                <p class="subtitle">Bottom tile</p>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child notification is-info">
+                <p class="title">Middle tile</p>
+                <p class="subtitle">With an image</p>
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-danger">
+              <p class="title">Wide tile</p>
+              <p class="subtitle">Aligned with the right tile</p>
+              <div class="content">
+                <!-- Content -->
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-success">
+            <div class="content">
+              <p class="title">Tall tile</p>
+              <p class="subtitle">With even more content</p>
+              <div class="content">
+                <!-- Content -->
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>`;
+     template2.innerHTML =
        `<section class="section" data-content="user-scene">
         <div class="table-container">
           <div class="search-bar" style="max-width: 280px; padding-bottom: 20px;">
