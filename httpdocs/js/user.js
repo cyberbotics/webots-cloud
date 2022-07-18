85b779179d5b2e15195324f95e874227a2070bce
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:58:20 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -89,9 +89,9 @@ export default class User extends Router {
           <div class="tile is-8 is-parent">
             <div class="tile is-child box">
               <p class="title">Three</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-              <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-              <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+              <div class="content">
+                ${projectsTable}
+              </div>
             </div>
           </div>
           <div class="tile is-vertical is-parent">
