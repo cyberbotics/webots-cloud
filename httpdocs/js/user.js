b7d4e04b314268d8933e3b9f51657c757b412511
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:37:08 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -31,27 +31,12 @@ export default class User extends Router {
       template.innerHTML =
       `<section class="section is-medium">
 <div class="tile is-ancestor">
-  <div class="tile is-vertical is-12">
-    <div class="tile">
-      <div class="tile is-parent is-vertical">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Vertical...</p>
-          <p class="subtitle">Top tile</p>
-        </article>
-        <article class="tile is-child notification is-warning">
-          <p class="title">...tiles</p>
-          <p class="subtitle">Bottom tile</p>
-        </article>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-info">
-          <p class="title">Middle tile</p>
-          <p class="subtitle">With an image</p>
-          <figure class="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/640x480.png">
-          </figure>
-        </article>
-      </div>
+  <div class="tile is-vertical is-8">
+    <div class="tile is-vertical">
+      <article class="tile is-child notification is-primary">
+        <p class="title">Projects</p>
+        <p class="subtitle">projects list goes here</p>
+      </article>
     </div>
     <div class="tile is-parent">
       <article class="tile is-child notification is-danger">
