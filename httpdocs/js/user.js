fe665c28112add6e5e89f1656e853979da3d69d8
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:46:32 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -88,7 +88,7 @@ export default class User extends Router {
 <div class="tile is-ancestor">
   <div class="tile is-vertical is-8">
     <div class="tile is-parent">
-      <article class="tile is-child notification box">
+      <article class="tile is-child notification is-warning box">
         <p class="title">My Projects</p>
         <div class="content">
           ${projectsTable}
