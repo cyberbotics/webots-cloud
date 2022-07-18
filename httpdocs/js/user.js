647a99c281b108a94f4e002308086a0eda477b27
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 13:56:26 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -410,6 +410,17 @@ export default class User extends Router {
         });
       });
     }
+    function findGetParameter(parameterName) {
+      let result = null;
+      let tmp = [];
+      let items = location.search.substr(1).split('&');
+      for (let index = 0; index < items.length; index++) {
+        tmp = items[index].split('=');
+        if (tmp[0] === parameterName)
+          result = decodeURIComponent(tmp[1]);
+      }
+      return result;
+    }
     function settingsPage() {
       // we need to be logged in to view this page
       if (!that.password || !that.email)
