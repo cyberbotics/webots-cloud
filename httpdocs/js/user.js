1d420e826cadd4141430906113cb04f59549641c
ThomasOliverKimble <tokimble@gmail.com>
2022-07-18 09:49:23 +0200

    my projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -85,76 +85,30 @@ export default class User extends Router {
 
       template.innerHTML =
       `<section class="section">
-<div class="tile is-ancestor">
-  <div class="tile is-vertical is-8">
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-warning box">
-        <p class="title">My Projects</p>
-        <div class="content">
-          ${projectsTable}
-        </div>
-      </article>
-    </div>
-  </div>
-  <div class="tile is-vertical is-parent">
-    <div class="tile is-child box is-success">
-      <p class="title">One</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
-        Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-    </div>
-    <div class="tile is-child box is-primary">
-      <p class="title">Two</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
-        Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-    </div>
-  </div>
-</div>
         <div class="tile is-ancestor">
           <div class="tile is-vertical is-8">
-            <div class="tile">
-              <div class="tile is-parent is-vertical">
-                <article class="tile is-child notification is-primary">
-                  <p class="title">Vertical...</p>
-                  <p class="subtitle">Top tile</p>
-                </article>
-                <article class="tile is-child notification is-warning">
-                  <p class="title">...tiles</p>
-                  <p class="subtitle">Bottom tile</p>
-                </article>
-              </div>
-              <div class="tile is-parent">
-                <article class="tile is-child notification is-info">
-                  <p class="title">Middle tile</p>
-                  <p class="subtitle">With an image</p>
-                  <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                  </figure>
-                </article>
-              </div>
-            </div>
             <div class="tile is-parent">
-              <article class="tile is-child notification is-danger">
-                <p class="title">Wide tile</p>
-                <p class="subtitle">Aligned with the right tile</p>
+              <article class="tile is-child notification is-warning box">
+                <p class="title">My Projects</p>
                 <div class="content">
-                  <!-- Content -->
+                  ${projectsTable}
                 </div>
               </article>
             </div>
           </div>
-          <div class="tile is-parent">
-            <article class="tile is-child notification is-success">
-              <div class="content">
-                <p class="title">Tall tile</p>
-                <p class="subtitle">With even more content</p>
-                <div class="content">
-                  <!-- Content -->
-                </div>
-              </div>
-            </article>
+          <div class="tile is-vertical is-parent">
+            <div class="tile is-child is-success box">
+              <p class="title">One</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
+                Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+            </div>
+            <div class="tile is-child is-primary box">
+              <p class="title">Two</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
+                Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+            </div>
           </div>
-        </div>
-      </section>`;
+        </div>`;
       that.setup('settings', [], template.content);
     }
     function resetPassword(id, token, email) {
