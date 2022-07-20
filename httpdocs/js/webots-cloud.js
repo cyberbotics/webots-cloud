47b9c8e67c9bd8bb2b4fdb7d92bc0b9a358c9a45
ThomasOliverKimble <tokimble@gmail.com>
2022-07-20 16:30:56 +0200

    clean up

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -706,11 +706,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function synchronizeSimulation(event) {
+      const searchString = getSearch('simulation');
       const id = event.target.id.substring(5);
       event.target.classList.add('fa-spin');
       const url = event.target.getAttribute('data-url');
-      fetch('ajax/project/create.php', {method: 'post',
-        body: JSON.stringify({url: url, id: id, search: getSearch('simulation')})})
+      fetch('ajax/project/create.php', {method: 'post', body: JSON.stringify({url: url, id: id, search: searchString})})
         .then(function(response) {
           return response.json();
         })
