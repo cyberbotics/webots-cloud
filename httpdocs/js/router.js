bb10bcba7651296c478230d77e0f94b95fba090b
ThomasOliverKimble <tokimble@gmail.com>
2022-07-26 15:28:53 +0200

    history testing

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -30,7 +30,6 @@ export default class Router {
     });
     window.onpopstate = function(event) {
       that.load(document.location.pathname + document.location.search + document.location.hash, false);
-      event.preventDefault();
     };
   }
   resetNavbar() {
