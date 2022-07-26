3d5e494ff96eca45d787878b4835439da4191a44
ThomasOliverKimble <tokimble@gmail.com>
2022-07-26 15:27:26 +0200

    history testing

--- httpdocs/js/router.js
+++ httpdocs/js/router.js
@@ -83,6 +83,7 @@ export default class Router {
         resolve();
       } else {
         let found = false;
+        console.log('scene search: ' + getSearches('scene'));
         for (let i = 0; i < that.routes.length; i++) {
           const route = that.routes[i];
           if (url.pathname === route.url) {
