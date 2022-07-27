e2a4bef04c300054c5f4879a398be9b70c3f76f8
ThomasOliverKimble <tokimble@gmail.com>
2022-07-27 10:37:46 +0200

    my-projects testing

--- httpdocs/js/user.js
+++ httpdocs/js/user.js
@@ -225,7 +225,7 @@ export default class User extends Router {
       console.log('page: ' + that.page);
       console.log('search: ' + that.search);
       console.log('sort: ' + that.sort);
-      console.log('type: ' + user);
+      console.log('type: ' + that.id);
       const pageLimit = 10;
       const user = parseInt(that.id);
       const offset = (that.page - 1) * pageLimit;
