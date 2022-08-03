18aaa40f09b479ab2c46af9cb1a4725e2c715800
ThomasOliverKimble <tokimble@gmail.com>
2022-08-03 08:29:14 +0200

    my-projects-delete-testing

--- httpdocs/ajax/animation/delete.php
+++ httpdocs/ajax/animation/delete.php
@@ -16,7 +16,6 @@ $mysqli->set_charset('utf8');
 $user = isset($data->user) ? intval($data->user) : 0;
 $animation = intval($data->animation);
 $password = isset($data->password) ? $mysqli->escape_string($data->password) : '';
-error("user: $user   //   id: $id");
 $query = "DELETE FROM animation WHERE id=$animation AND (user=0 OR user IN (SELECT id FROM user WHERE id=$user AND password='$password') OR EXISTS (SELECT * FROM user WHERE id=$user AND password='$password' AND email LIKE '%@cyberbotics.com'))";
 $mysqli->query($query) or error($mysqli->error);
 if ($mysqli->affected_rows === 0)
