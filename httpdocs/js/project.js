7f53728a2d855163342172bccce2e77a9e469cfd
ThomasOliverKimble <tokimble@gmail.com>
2022-08-05 11:31:20 +0200

    revert to testing

--- httpdocs/js/project.js
+++ httpdocs/js/project.js
@@ -128,7 +128,7 @@ export default class Project extends User {
     const mode = this.findGetParameter('mode');
     const version = (fallbackVersion && fallbackVersion !== 'undefined') ? fallbackVersion :
       (data ? data.version : this.findGetParameter('version'));
-    const src = 'https://cyberbotics.com/wwi/testingRelative/WebotsView.js';
+    const src = 'https://cyberbotics.com/wwi/testing' + version + '/WebotsView.js';
 
     if (!data)
       that._updateSimulationViewCount(url);
