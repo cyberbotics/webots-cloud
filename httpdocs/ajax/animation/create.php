bd218ee5863410674b002a982000a41c9b4c3831
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-09-08 15:30:19 +0200

    add quote

--- httpdocs/ajax/animation/create.php
+++ httpdocs/ajax/animation/create.php
@@ -141,7 +141,7 @@
   }
   $branch = "testing";
   $query = "INSERT INTO animation(title, description, version, duration, size, user, branch) ".
-           "VALUES(\"$escaped_title\", \"$escaped_description\", \"$escaped_version\", $duration, $size, $user, $branch)";
+           "VALUES(\"$escaped_title\", \"$escaped_description\", \"$escaped_version\", $duration, $size, $user, \"$branch\")";
   $mysqli->query($query) or error($mysqli->error);
   $id = $mysqli->insert_id;
 
