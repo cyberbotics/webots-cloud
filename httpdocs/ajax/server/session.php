d7d82571a84a2c1af9b6d8a84c84bdbbe3750de0
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-09-09 15:40:10 +0200

    ambiguous

--- httpdocs/ajax/server/session.php
+++ httpdocs/ajax/server/session.php
@@ -21,7 +21,7 @@ $branch = basename(dirname(__FILE__, 4));
 $select = "SELECT url FROM server JOIN server_branch ON server.id=server_branch.id";
 $extra_condition = "branch=\"$branch\"";
 # search for a dedicated server first
-$query = "$select WHERE `load` < 100 AND id IN (SELECT server FROM repository WHERE \"$url%\" LIKE CONCAT(url, '%')) AND $extra_condition ORDER BY `load` LIMIT 1";
+$query = "$select WHERE `load` < 100 AND server.id IN (SELECT server FROM repository WHERE \"$url%\" LIKE CONCAT(url, '%')) AND $extra_condition ORDER BY `load` LIMIT 1";
 $result = $mysqli->query($query) or error($mysqli->error);
 if ($row = $result->fetch_array(MYSQLI_ASSOC))
   return_url($row['url']);
