d1f64a1553aab287c7a47f7615b02e04c225f2f0
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-09-29 10:13:10 +0200

    Update webots-cloud.js

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -970,7 +970,7 @@ document.addEventListener('DOMContentLoaded', function() {
             for (let i = 0; i < data.animations.length; i++)
               line += '<tr>' + animationRow(data.animations[i]) + '</tr>';
             if (data.animations.length != pageLimit)  // add extra space
-              line += '<tr style="height=' + 50 * (pageLimit - data.animations.length) + 'px"></tr>';
+              line += '<tr style="height:' + 50 * (pageLimit - data.animations.length) + 'px"></tr>';
             let parent = project.content.querySelector(`section[data-content="${typeName}"] > div > table > tbody`);
             parent.innerHTML = line;
             for (let i = 0; i < data.animations.length; i++) {
