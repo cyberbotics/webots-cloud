d445b63e8123c2185e5d7d25ac849ed598dfc9fa
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-09-29 08:20:07 +0200

    typo

--- httpdocs/ajax/project/create.php
+++ httpdocs/ajax/project/create.php
@@ -97,7 +97,7 @@ else
 $mysqli->query($query) or error($mysqli->error);
 if ($mysqli->affected_rows != 1) {
   if ($id === 0)
-    error("This simulation already exists branch.");
+    error("This simulation already exists.");
   else
     error("Failed to update the simulation.");
 }
