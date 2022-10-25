7c1c3113b1b0fca515369ff89e39c4575c14501a
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-10-25 10:29:42 +0200

    debug print

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -1203,6 +1203,7 @@ document.addEventListener('DOMContentLoaded', function() {
       fetch('/ajax/proto/list.php', {method: 'post',
         body: JSON.stringify({offset: offset, limit: pageLimit, sortBy: sortBy, search: searchString})})
         .then(function(response) {
+          console.log(response.text())
           return response.json();
         })
         .then(function(data) {
