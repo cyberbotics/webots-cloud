b8746e00fbba0673ac896ca9efce55ae657aade0
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-12-13 16:49:35 +0100

    Update benchmark.php

--- httpdocs/benchmark.php
+++ httpdocs/benchmark.php
@@ -1,19 +1,30 @@
 <?php
-# curl -d '{"event_type": "run_trigger", "client_payload": {"repository": "${{ github.repository }}"}}' -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.everest-preview+json" "https://api.github.com/repos/omichel/my-benchmark/dispatches"
 
-function get_json($url){
-  $base = "https://api.github.com";
+include '../php/token.php';
+
+function repository_dispatch($organizer_repository, $participant_repository) {
+  global $token;
   $curl = curl_init();
-  curl_setopt($curl, CURLOPT_URL, $base . '/' . $url);
+  $url = 'https://api.github.com/repos/' . $organizer_repository . '/dispatches';
+  curl_setopt($curl, CURLOPT_URL, $url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($curl, CURLOPT_USERAGENT, 'webots.cloud');
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Authorization: token ' . $token,
+    'Accept: application/vnd.github.everest-preview+json'
+  ]);
+  curl_setopt($curl, CURLOPT_POST, 1);
+  curl_setopt($curl, CURLOPT_POSTFIELDS,'{"event_type": "run_trigger", "client_payload": {"repository": "'. $participant_repository .'"}}');
   $content = curl_exec($curl);
-  echo $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  if ($answer = curl_getinfo($curl, CURLINFO_HTTP_CODE) != 204)  # 204: no answer
+    echo('Unexpected answer: ' . $answer. ' on ' . $url . '<br><br>' . $content);
+  else
+    echo 'OK';
   curl_close($curl);
-  return $content;
 }
 
-echo get_json("users/omichel/repos");
+repository_dispatch($_GET['organizer'], $_GET['participant']);
 
 ?>
