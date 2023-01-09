48a3bf35c3e8a11a6d739bf7e14f878e0362ca00
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-01-09 17:19:18 +0100

    Fixed wrong project id in queue

--- httpdocs/run_competition.php
+++ httpdocs/run_competition.php
@@ -106,12 +106,12 @@ $participant = $mysqli->real_escape_string($participant);
 $query = "SELECT id FROM project WHERE `url` LIKE 'https://github.com/$organizer/%'";
 $result = $mysqli->query($query) or die($mysqli->error);
 $project = $result->fetch_array(MYSQLI_ASSOC);
-$project_id = $project['id'];
+$project_id = intval($project['id']);
 
 if (isset($_POST['organizer_repo_token'])) {
   if (!check_repo_token($_POST['organizer_repo_token'], $organizer))
     die("Error: wrong organizer repository token.");
-  $query = "DELETE FROM queue WHERE queue.project=$project_id AND queue.participant = 'R:$participant'";
+  $query = "DELETE FROM queue WHERE project=$project_id AND participant='R:$participant'";
   $mysqli->query($query) or die($mysqli->error);
   if ($mysqli->affected_rows === 0)
     die("Error: could not delete repository dispatch queue for $participant");
@@ -142,7 +142,7 @@ if ($total == 0)
   $running = 'R:';
 else
   $running = '';
-$query = "INSERT IGNORE INTO queue(project, participant) VALUES((SELECT id FROM project WHERE url LIKE 'https://github.com/$organizer/%'), '$running$participant')";
+$query = "INSERT IGNORE INTO queue(project, participant) VALUES($project_id, '$running$participant')";
 $mysqli->query($query) or die($mysqli->error);
 
 if ($total == 0)
