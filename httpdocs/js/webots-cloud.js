9877ec2480aafe944f6309dc0c0c9c2ffb984ebd
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-11 11:20:16 +0100

    license

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -1581,7 +1581,7 @@ document.addEventListener('DOMContentLoaded', function() {
           line.startsWith('#documentation url:'))
           continue;
 
-        console.log(version)
+        console.log(license)
         const infoGrid = document.createElement('div');
         infoGrid.className = 'proto-info-array';
 
