b8765a509510d594c8774992de8f8e12c03583a3
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-01-12 10:33:32 +0100

    next

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -1701,7 +1701,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         //remove enumeration
         matches = fieldsDefinition.matchAll(cleaningRegex);
-        console.log(matches.next().next())
+        console.log(matches.next().hasNext())
         const removeCommentRegex = /\s*(#.*)/ig;
         const removeInitialFieldRegex = /^\s*.*field\s/ig;
         for (const match of matches) {
