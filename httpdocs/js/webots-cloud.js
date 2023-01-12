a6a575c5f69e8fa7e0d09851025f298dfa7b6efd
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-01-12 12:35:23 +0100

    corrected and complete

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -1562,13 +1562,13 @@ document.addEventListener('DOMContentLoaded', function() {
               fetch(url)
                 .then(response => response.text())
                 .then(content => {
-                  createMdFromProto(protoURl, proto, true);
+                  createMdFromProto(protoURl, proto, protoName, true);
                 });
             });
         });
     }
-
-    function createMdFromProto(protoURl, proto, generateAll) {
+    
+    function createMdFromProto(protoURl, proto, protoName, generateAll) {
       // parse header
       let version, license, licenseUrl;
       let description = '';
@@ -1576,7 +1576,7 @@ document.addEventListener('DOMContentLoaded', function() {
       for (const line of proto.split('\n')) {
         if (!line.startsWith('#'))
           break;
-
+    
         if (line.startsWith('#VRML_SIM') || line.startsWith('# VRML_SIM'))
           version = line.substring(line.indexOf('VRML_SIM') + 9).split(' ')[0];
         else if (line.startsWith('# license:') || line.startsWith('#license:'))
@@ -1593,17 +1593,17 @@ document.addEventListener('DOMContentLoaded', function() {
           description += newLine + '\n';
         }
       }
-
+    
       const infoGrid = document.createElement('div');
       infoGrid.className = 'proto-info-array';
-
+    
       const versionP = document.createElement('p');
       versionP.textContent = 'Version';
       versionP.className = 'info-array-cell first-column-cell first-row-cell';
       versionP.style.gridRow = 1;
       versionP.style.gridColumn = 1;
       infoGrid.appendChild(versionP);
-
+    
       const versionContentA = document.createElement('a');
       versionContentA.textContent = version;
       versionContentA.href = 'https://github.com/cyberbotics/webots/releases/tag/' + version;
@@ -1612,7 +1612,7 @@ document.addEventListener('DOMContentLoaded', function() {
       versionContentA.style.gridRow = 1;
       versionContentA.style.gridColumn = 2;
       infoGrid.appendChild(versionContentA);
-
+    
       const licenseP = document.createElement('p');
       licenseP.textContent = 'License';
       licenseP.className = 'info-array-cell first-column-cell';
@@ -1620,7 +1620,7 @@ document.addEventListener('DOMContentLoaded', function() {
       licenseP.style.gridColumn = 1;
       licenseP.style.backgroundColor = '#fafafa';
       infoGrid.appendChild(licenseP);
-
+    
       const licenseContentA = document.createElement('a');
       licenseContentA.textContent = license;
       licenseContentA.className = 'info-array-cell last-column-cell';
@@ -1630,14 +1630,14 @@ document.addEventListener('DOMContentLoaded', function() {
       licenseContentA.style.gridRow = 2;
       licenseContentA.style.gridColumn = 2;
       infoGrid.appendChild(licenseContentA);
-
+    
       const sourceP = document.createElement('p');
       sourceP.textContent = 'Source';
       sourceP.className = 'info-array-cell first-column-cell';
       sourceP.style.gridRow = 3;
       sourceP.style.gridColumn = 1;
       infoGrid.appendChild(sourceP);
-
+    
       const sourceContentA = document.createElement('a');
       sourceContentA.href = protoURl;
       sourceContentA.className = 'info-array-cell last-column-cell';
@@ -1646,21 +1646,22 @@ document.addEventListener('DOMContentLoaded', function() {
       sourceContentA.style.gridRow = 3;
       sourceContentA.style.gridColumn = 2;
       infoGrid.appendChild(sourceContentA);
-
+    
       if (generateAll) {
-        const fieldRegex = /\[\n((.*\n)*)\]/ig;
+        const fieldRegex = /\[\n((.*\n)*)\]/mg;
         let matches = proto.matchAll(fieldRegex);
         let fieldsDefinition;
         const fieldEnumeration = new Map();
         const describedField = [];
         let fields = '';
+        let file = '';
         for (const match of matches) {
           fieldsDefinition = match[1];
           break;
         }
-
+    
         // remove enumerations
-        const removeEnumRegex = /.*ield\s+([^ ]*?)(\{(?:[^\[\n]*\,?\s?)(?<!(\{))\})\s+([^ ]*)\s+([^#\n]*)(#?)(.*)/ig;
+        const removeEnumRegex = /.*ield\s+([^ ]*?)(\{(?:[^\[\n]*\,?\s?)(?<!(\{))\})\s+([^ ]*)\s+([^#\n]*)(#?)(.*)/mg;
         matches = fieldsDefinition.matchAll(removeEnumRegex);
         for (const match of matches) {
           fieldEnumeration.set(match[4], match[2].slice(1, -1).split(','));
@@ -1674,7 +1675,8 @@ document.addEventListener('DOMContentLoaded', function() {
               fieldsDefinition = fieldsDefinition.replace(match[2], '');
           }
         }
-        const spacingRegex = /.*ield\s+([^ ]*?)(\s+)([^ ]*)\s+([^#\n]*)(#?)(.*)/ig;
+    
+        const spacingRegex = /.*ield\s+([^ ]*?)(\s+)([^ ]*)\s+([^#\n]*)(#?)(.*)/mg;
         matches = fieldsDefinition.matchAll(spacingRegex);
         let minSpaces = 2000;
         for (const match of matches) {
@@ -1683,10 +1685,10 @@ document.addEventListener('DOMContentLoaded', function() {
             minSpaces = spaces.length;
         }
         const spacesToRemove = Math.max(minSpaces - 2, 0);
-
-        const cleaningRegex = /^\s*(.*?ield)\s+([^ \{]*)(\s+)([^ ]*)\s+([^#\n]*)(#?)(.*)((\n*(    |  \]).*)*)/ig;
-        const isDescriptionRegex = /Is\s`([a-zA-Z]*).([a-zA-Z]*)`./ig;
-
+    
+        const cleaningRegex = /^\s*(.*?ield)\s+([^ \{]*)(\s+)([^ ]*)\s+([^#\n]*)(#?)(.*)((\n*( {4}| {2}\]).*)*)/gm;
+        const isDescriptionRegex = /Is\s`([a-zA-Z]*).([a-zA-Z]*)`./g;
+    
         const baseNodeList = ['WorldInfo', 'Hinge2JointParameters', 'PBRAppearance', 'ContactProperties', 'SolidReference',
           'Charger', 'Capsule', 'Mesh', 'Background', 'BallJoint', 'Focus', 'RotationalMotor', 'ElevationGrid', 'Pen',
           'Cylinder', 'GPS', 'SliderJoint', 'Compass', 'Emitter', 'Track', 'Cone', 'LED', 'Slot', 'Radar', 'Coordinate',
@@ -1699,15 +1701,14 @@ document.addEventListener('DOMContentLoaded', function() {
           'LightSensor', 'Gyro', 'Receiver', 'Microphone', 'Solid', 'Billboard', 'Fluid', 'Muscle', 'Group', 'Skin',
           'Material'];
 
-        //remove enumeration
+        // remove enumeration
         matches = fieldsDefinition.matchAll(cleaningRegex);
-        console.log([...matches])
-        const removeCommentRegex = /\s*(#.*)/ig;
-        const removeInitialFieldRegex = /^\s*.*field\s/ig;
+
+        const removeCommentRegex = /\s*(#.*)/mg;
+        const removeInitialFieldRegex = /^\s*.*field\s/mg;
         for (const match of matches) {
           if (!(match[1].includes('hiddenField') || match[1].includes('deprecatedField'))) {
             const fieldType = match[2];
-            const spaces = match[3];
             const fieldName = match[4];
             const fieldComment = match[7].trim();
             // skip 'Is `NodeType.fieldName`.' descriptions
@@ -1716,7 +1717,7 @@ document.addEventListener('DOMContentLoaded', function() {
               // add link to base nodes:
               for (const baseNode of baseNodeList) {
                 if (fieldComment.indexOf(baseNode) !== -1) {
-                  const link = ' [' + baseNode + '](https://cyberbotics.com/doc/reference/' + baseNode.toLowerCase()+')';
+                  const link = ' [' + baseNode + '](https://cyberbotics.com/doc/reference/' + baseNode.toLowerCase() + ')';
                   fieldComment.replace(' ' + baseNode, link);
                 }
               }
@@ -1733,11 +1734,68 @@ document.addEventListener('DOMContentLoaded', function() {
             if (spacesToRemove > 0)
               fieldString = fieldString.replace(fieldType + ' '.repeat(spacesToRemove), fieldType);
 
-            fields += fieldString;
+            fields += fieldString + '\n';
           }
         }
+        fetch('https://cyberbotics.com/wwi/proto/protoVisualizer/temporary-proto-list.xml')
+          .then(result => result.text())
+          .then(content => {
+            const xml = new window.DOMParser().parseFromString(content, 'text/xml');
+            const protos = xml.getElementsByTagName('proto');
+            let protoNode;
+            for (const proto of protos) {
+              if (proto.getElementsByTagName('name')[0].textContent === protoName) {
+                protoNode = proto;
+                break;
+              }
+            }
+            const baseType = protoNode.getElementsByTagName('base-type')[0].textContent;
 
-        console.log(fieldsDefinition)
+            file += description + '\n';
+            file += 'Derived from [' + baseType + '](https://cyberbotics.com/doc/reference/' + baseType.toLowerCase() + '.\n\n';
+            file += '```\n';
+            file += protoName + ' {\n';
+            file += fields;
+            file += '}\n';
+            file += '```\n\n';
+
+            if (describedField) {
+              file += ' ### ' + protoName + ' Field Summary\n\n';
+              for (const [fieldType, fieldName, fielDescription] of describedField) {
+                file += '- ' + fieldName + ' : ' + fielDescription;
+                const isMFField = fieldType.startsWith('MF');
+                if (fieldEnumeration.has(fieldName)) {
+                  const values = fieldEnumeration.get(fieldName);
+                  if (isMFField)
+                    file += ' This field accept a list of ';
+                  else {
+                    if (values.length > 1)
+                      file += ' This field accepts the following values: ';
+                    else
+                      file += ' This field accepts the following value: ';
+                  }
+
+                  for (let i = 0; i < values.length; i++) {
+                    const value = values[i].split('{')[0]; // In case of node keep only the type
+                    if (i === values.length - 1) {
+                      if (isMFField)
+                        file += '`' + value.trim() + '` ' + fieldType.replace('MF', '').toLowerCase() + 's.';
+                      else
+                        file += '`' + value.trim() + '`.';
+                    } else if (i === values.length - 2) {
+                      if (values.length === 2)
+                        file += '`' + value.trim() + '` and ';
+                      else
+                        file += '`' + value.trim() + '`, and ';
+                    } else
+                      file += '`' + value.trim() + ', ';
+                  }
+                }
+                file += '\n\n';
+              }
+            }
+            console.log(file)
+          });
       }
       return infoGrid;
     }
