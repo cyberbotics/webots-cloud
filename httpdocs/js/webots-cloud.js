32555ee71dc8b2405d0b2902019095767fbf9221
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-13 08:06:56 +0100

    test

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -1750,7 +1750,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
 
           const baseType = protoNode.getElementsByTagName('base-type')[0].textContent;
-          const description = protoNode.getElementsByTagName('description')[0].textContent.replace('\n', '</br>');
+          const description = protoNode.getElementsByTagName('description')[0].textContent.replace('\\n', '</br>');
           console.log(description)
           file += description + '\n\n';
           file += 'Derived from [' + baseType + '](https://cyberbotics.com/doc/reference/' + baseType.toLowerCase() + ').\n\n';
