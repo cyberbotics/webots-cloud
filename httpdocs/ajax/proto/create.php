a34b0a7121e5f1cdf6ed49425962aad6e7f5eab6
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-16 12:15:21 +0100

    correct

--- httpdocs/ajax/proto/create.php
+++ httpdocs/ajax/proto/create.php
@@ -88,8 +88,8 @@ $row = $result->fetch_array(MYSQLI_ASSOC);
 $viewed = ($result && $row) ? $row['viewed'] : 0;
 $branch = basename(dirname(__FILE__, 4));
 if ($id === 0)
-  $query = "INSERT IGNORE INTO proto(url, viewed, stars, title, description, version, branch) "
-          ."VALUES(\"$url\", $viewed, $stars, \"$title\", \"$description\", \"$version\", \"$branch\", \"$license\")";
+  $query = "INSERT IGNORE INTO proto(url, viewed, stars, title, description, version, branch, license_url, license) "
+          ."VALUES(\"$url\", $viewed, $stars, \"$title\", \"$description\", \"$version\", \"$branch\", \"$license_url\". \"$license\")";
 else
   $query = "UPDATE proto SET viewed=$viewed, stars=$stars, title=\"$title\", description=\"$description\", "
           ."version=\"$version\", updated=NOW() "
