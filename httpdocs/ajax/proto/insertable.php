0e350e6f248d824ed97407c8ac9a8451526603e9
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-17 12:35:59 +0100

    slot_type

--- httpdocs/ajax/proto/insertable.php
+++ httpdocs/ajax/proto/insertable.php
@@ -18,6 +18,9 @@
   $condition = "branch=\"$branch\" AND base_type IN ('"
      . implode("','", array_map(fn($string): string => mysqli_real_escape_string($mysqli, $string),$base_types))
      . "')";
+  $slot_type = $data->slotType;
+  if ($slot_type)
+    $condition .= " AND slot_type=".$mysqli->escape_string($slot_type);
   $result = $mysqli->query("SELECT * FROM proto WHERE $condition");
   $protos = array();
   while($row = $result->fetch_assoc()) {
