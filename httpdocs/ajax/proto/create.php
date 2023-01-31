5afb4d33d7609e2fd6063e825a69c7fe7498ba51
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-31 11:43:23 +0100

    typo

--- httpdocs/ajax/proto/create.php
+++ httpdocs/ajax/proto/create.php
@@ -206,7 +206,7 @@ function get_parent($externprotos, $base_proto, $parent_url) {
       $extern_url = $externprotos[$i][1];
       if (str_starts_with($extern_url, "webots://"))
         $extern_url = str_replace("webots://", "https://github.com/cyberbotics/webots/blob/released/", $extern_url);
-      else if (!srt_starts_with($extern_url, "https"))
+      else if (!str_starts_with($extern_url, "https"))
         $extern_url = substr($parent_url, 0, strrpos($parent_url, '/') + 1).$extern_url;
       $parent_url = $extern_url;
       $check_url = proto_check_url($extern_url);
