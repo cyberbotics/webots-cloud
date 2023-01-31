14d27b79790d130dc3c03bb624552e7176a22f65
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-01-31 11:12:59 +0100

    test

--- httpdocs/ajax/proto/create.php
+++ httpdocs/ajax/proto/create.php
@@ -243,7 +243,7 @@ function get_parent($externprotos, $base_proto, $parent_url) {
   if (!$found_parent) {
     $str = '';
     for($i = 0; $i < count($externprotos); $i++) {
-      $str .= $externprotos[i][0];
+      $str .= $externprotos[$i][0];
     }
     error("$str base: $base_proto parent_url: $parent_url");
   }
