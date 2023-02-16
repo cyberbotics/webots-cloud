e00307a0a0d038279a95361e4c310b27a4c9ed96
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-02-16 12:04:07 +0100

    test

--- httpdocs/ajax/proto/needsRobotAncestor.php
+++ httpdocs/ajax/proto/needsRobotAncestor.php
@@ -18,5 +18,5 @@
   $condition = "branch=\"$branch\" AND url=\"$url\"";
 
   $result = $mysqli->query("SELECT needs_robot_ancestor FROM proto WHERE $condition");
-  die(json_encode($result->fetch_array(MYSQLI_ASSOC)));
+  die($result);
  ?>
