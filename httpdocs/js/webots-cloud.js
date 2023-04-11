37ec2fc1192c755519676d39f5205355055ea15d
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-04-11 15:10:13 +0200

    value

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -717,8 +717,7 @@ ${deleteProject}`;
     }
 
     function sortProtoBy(sort) {
-      console.log(sort)
-      console.log(sort.srcElement[0].value)
+      console.log(sort.options[sort.selectedIndex].value)
       setSorts('proto', sort.target[0].value);
       listProtos(protoPage, getSort('proto'), getSearch('proto'));
     }
