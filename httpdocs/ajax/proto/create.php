becdee2ee361de12c7ad7df61f77ff283c89bcc5
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-04-18 11:12:31 +0200

    no ignore

--- httpdocs/ajax/proto/create.php
+++ httpdocs/ajax/proto/create.php
@@ -191,7 +191,7 @@ $id = ($id === 0) ? $mysqli->insert_id : $id;
 
 if ($remove_old_tag)
   $query = $mysqli->query("DELETE FROM proto_tagmap WHERE proto_id = $id AND tag_id NOT IN (SELECT tag_id FROM tags WHERE name IN ('$keywords'))");
-$query = $mysqli->query("INSERT IGNORE INTO proto_tagmap (proto_id, tag_id) SELECT $id, tag_id FROM proto_tag WHERE name IN ('$keywords')") or error($mysqli->error);
+$query = $mysqli->query("INSERT INTO proto_tagmap (proto_id, tag_id) SELECT $id, tag_id FROM proto_tag WHERE name IN ('$keywords')") or error($mysqli->error);
 
 # return answer
 $search = isset($data->search) ? $data->search : "";
