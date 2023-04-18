d08982db7308f5586e47b4aad70acf6a6f7f3241
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-04-18 09:47:47 +0200

    test

--- httpdocs/ajax/proto/create.php
+++ httpdocs/ajax/proto/create.php
@@ -185,9 +185,9 @@ if ($mysqli->affected_rows != 1) {
 }
 
 $id = ($id === 0) ? $mysqli->insert_id : $id;
-$query = "INSERT IGNORE INTO proto_tagmap (proto_id, tag_id) SELECT $id, tag_id FROM proto_tag WHERE name IN ('$keywords')";
-$query = $mysqli->query($query) or error($mysqli->error);
-die($query);
+$query_string = "INSERT IGNORE INTO proto_tagmap (proto_id, tag_id) SELECT $id, tag_id FROM proto_tag WHERE name IN ('$keywords')";
+$query = $mysqli->query($query_string) or error($mysqli->error);
+die($query_string);
 # return answer
 $search = isset($data->search) ? $data->search : "";
 $condition = "branch=\"$branch\"";
