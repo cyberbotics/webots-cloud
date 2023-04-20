4b943ea6a67874a3dab93f08f235cfe8eaff8802
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-04-20 16:55:12 +0200

    test

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -655,7 +655,7 @@ ${deleteProject}`;
           </section>
           <section class="section${(activeTab === 'proto') ? ' is-active' : ''}" data-content="proto">
             <div class="first-tag-container">
-              <div class="first-tag">Robot</div>
+              <div class="first-tag"><img src="../images/profile.png"></img>Robot</div>
               <div class="first-tag">Sensor</div>
               <div class="first-tag">Industrial</div>
               <div class="first-tag">Household</div>
