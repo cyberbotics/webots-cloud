6324b07e09726a22bdf5407501bedc8246a0ddfb
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-02 12:47:25 +0200

    furniture and factory

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -674,7 +674,7 @@ ${deleteProject}`;
               <div class="first-level-keyword" title="industrial"><img src="../images/proto/factory.png"><p>Industrial</p></div>
               <div class="first-level-keyword" title="household"><img src="../images/proto/household.png"><p>Household</p></div>
               <div class="first-level-keyword" title="vehicle"><img src="../images/proto/vehicle.png"><p>Vehicle</p></div>
-              <div class="first-level-keyword" title="furniture"><img src="../images/profile.png"><p>Furniture</p></div>
+              <div class="first-level-keyword" title="furniture"><img src="../images/proto/furniture.png"><p>Furniture</p></div>
               <div class="first-level-keyword" title="building"><img src="../images/profile.png"><p>Building</p></div>
               <div class="first-level-keyword" title="animal"><img src="../images/profile.png"><p>Animal</p></div>
               <div class="first-level-keyword" title="exterior"><img src="../images/profile.png"><p>Exterior</p></div>
