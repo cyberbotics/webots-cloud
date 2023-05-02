bb53af8eeb5d789381c0035ad665f559274be823
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-02 15:40:52 +0200

    add password

--- httpdocs/ajax/proto/refresh_protos.php
+++ httpdocs/ajax/proto/refresh_protos.php
@@ -1,15 +1,19 @@
 <?php # This script refresh all protos
-require '../../../php/github_oauth.php';
-require '../../../php/github_asset.php';
-require '../../../php/database.php';
-require '../../../php/create_or_update.php';
+
 require '../../../php/refresh_password.php';
-$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
-if ($mysqli->connect_errno)
-  error("Can't connect to MySQL database: $mysqli->connect_error");
-$mysqli->set_charset('utf8');
-$result = $mysqli->query('SELECT id, url FROM proto where branch="proto"') or error($mysqli->error);
-while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-  create_or_update_proto($row['url'], $row['id'], '');
+if ($_GET['password'] === $refresh_password) {
+  require '../../../php/github_oauth.php';
+  require '../../../php/github_asset.php';
+  require '../../../php/database.php';
+  require '../../../php/create_or_update.php';
+
+  $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
+  if ($mysqli->connect_errno)
+    error("Can't connect to MySQL database: $mysqli->connect_error");
+  $mysqli->set_charset('utf8');
+  $result = $mysqli->query('SELECT id, url FROM proto where branch="proto"') or error($mysqli->error);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    create_or_update_proto($row['url'], $row['id'], '');
+  }
 }
 ?>
