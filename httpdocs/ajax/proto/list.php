e278a6831ae63974515db08a69f653616fbd09f9
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-05-11 16:49:51 +0200

    pagination

--- httpdocs/ajax/proto/list.php
+++ httpdocs/ajax/proto/list.php
@@ -33,19 +33,24 @@
     $condition .= " AND LOWER(title) LIKE LOWER('%$searchString%')";
   }
 
+  $query = "SELECT * FROM proto ";
+  $query_count = "SELECT COUNT(*) AS count FROM proto";
   if (isset($data->keyword) && $data->keyword !== '') {
     $keyword = $mysqli->escape_string($data->keyword);
     #keyword with parent
     if (isset($data->keyword_parent) && $data->keyword_parent !== '') {
       $keyword_parent = $mysqli->escape_string($data->keyword_parent);
-      $query = "SELECT * FROM proto JOIN proto_keywordmap on proto.id=proto_keywordmap.proto_id JOIN proto_keyword as child on proto_keywordmap.keyword_id=child.keyword_id LEFT JOIN proto_keyword as parent on child.parent_id=parent.keyword_id WHERE (parent.name=\"$keyword_parent\" AND child.name=\"$keyword\") AND ";
+      $middle_part = "JOIN proto_keywordmap on proto.id=proto_keywordmap.proto_id JOIN proto_keyword as child on proto_keywordmap.keyword_id=child.keyword_id LEFT JOIN proto_keyword as parent on child.parent_id=parent.keyword_id WHERE (parent.name=\"$keyword_parent\" AND child.name=\"$keyword\") AND ";
     }
     # the keyword has no parent
     else
-      $query = "SELECT * FROM proto JOIN proto_keywordmap on proto.id=proto_keywordmap.proto_id JOIN proto_keyword as child on proto_keywordmap.keyword_id=child.keyword_id LEFT JOIN proto_keyword as parent ON child.parent_id=parent.keyword_id WHERE (parent.name=\"$keyword\" OR child.name=\"$keyword\") AND ";
+      $middle_part .= "JOIN proto_keywordmap on proto.id=proto_keywordmap.proto_id JOIN proto_keyword as child on proto_keywordmap.keyword_id=child.keyword_id LEFT JOIN proto_keyword as parent ON child.parent_id=parent.keyword_id WHERE (parent.name=\"$keyword\" OR child.name=\"$keyword\") AND ";
   } else
-    $query = "SELECT * FROM proto WHERE ";
+    $middle_part .= "WHERE ";
 
+  $query .= $middle_part;
+  $query_count -= $middle_part;
+  
   $offset = isset($data->offset) ? intval($data->offset) : 0;
   $limit = isset($data->limit) ? intval($data->limit) : 10;
   $query .= "$condition GROUP BY proto.id ORDER BY $parameter $order LIMIT $limit OFFSET $offset";
@@ -60,7 +65,7 @@
     $row['version'] = htmlentities($row['version']);
     array_push($protos, $row);
   }
-  $result = $mysqli->query("SELECT COUNT(*) AS count FROM proto WHERE $condition") or error($mysqli->error);
+  $result = $mysqli->query($query_count) or error($mysqli->error);
   $count = $result->fetch_array(MYSQLI_ASSOC);
   $answer = new StdClass;
   $answer->protos = $protos;
