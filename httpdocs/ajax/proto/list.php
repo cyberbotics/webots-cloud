49f7dee0fa585fc3b0c960a1cbdcd40cdedf9c3f
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-17 11:36:36 +0200

    simplify

--- httpdocs/ajax/proto/list.php
+++ httpdocs/ajax/proto/list.php
@@ -54,10 +54,8 @@
 
   $offset = isset($data->offset) ? intval($data->offset) : 0;
   $limit = isset($data->limit) ? intval($data->limit) : 10;
-  $end_part = "$condition GROUP BY proto.id ORDER BY $parameter";
-  $query .= $end_part;
-  $query .= " $order LIMIT $limit OFFSET $offset";
-  $query_count .= $end_part;
+  $query .= "$condition GROUP BY proto.id ORDER BY $parameter $order LIMIT $limit OFFSET $offset";
+  $query_count .= $condition;
   $result = $mysqli->query($query) or error($mysqli->error);
   $protos = array();
   die($query_count);
