e13db585dc48251a1fcef0b9d75e95f0ac31787c
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-22 12:22:10 +0200

    print

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -223,6 +223,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updatePagination(tab, current, max) {
+      console.log("update pagination")
       const hrefSort = getSort(tab) && getSort(tab) !== 'default' ? '?sort=' + getSort(tab) : '';
       const hrefSearch = getSearch(tab) && getSearch(tab) !== '' ? '?search=' + getSearch(tab) : '';
       const nav = document.querySelector(`section[data-content="${tab}"] > nav`);
