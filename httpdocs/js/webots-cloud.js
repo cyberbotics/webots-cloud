224df56700fb86c658b88a61a008364684f8b402
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-23 11:53:55 +0200

    type

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -883,7 +883,7 @@ ${deleteProject}`;
         url.searchParams.append('sort', getSort(type));
       if (getSearch(type) && getSearch(type) !== '')
         url.searchParams.append('search', getSearch(type));
-
+      console.log(type)
       if (type === 'proto')
         console.log(keywordSearch)
       window.history.replaceState(null, '', (url.pathname + url.search).toString());
