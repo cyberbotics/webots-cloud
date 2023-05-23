8559557f75465d7fe424262de88b61414bd67237
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-05-23 11:45:57 +0200

    remove print

--- httpdocs/js/webots-cloud.js
+++ httpdocs/js/webots-cloud.js
@@ -874,7 +874,6 @@ ${deleteProject}`;
     }
 
     function searchAndSortTable(type, isSearch) {
-      console.log("search and sort table")
       const url = new URL(document.location.origin + document.location.pathname);
       if (getPage(type) !== 1 && !isSearch)
         url.searchParams.append('p', getPage(type));
