866e039c62b63f2f82f3533a7734a368f951e03b
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-05-11 22:59:50 +0200

    Update setup.php

--- httpdocs/ajax/server/setup.php
+++ httpdocs/ajax/server/setup.php
@@ -2,26 +2,6 @@
   function error($message) {
     die("{\"error\":\"$message\"}");
   }
-  function gethostbynamel6($host) { # adapted from https://www.php.net/manual/en/function.gethostbyname.php
-    $dns6 = dns_get_record($host, DNS_AAAA);
-    $dns4 = dns_get_record($host, DNS_A);
-    $dns = array_merge($dns4, $dns6);
-    $ip6 = array();
-    $ip4 = array();
-    foreach ($dns as $record) {
-      if ($record["type"] == "A")
-        $ip4[] = $record["ip"];
-      if ($record["type"] == "AAAA")
-        $ip6[] = $record["ipv6"];
-    }
-    if (count($ip6) < 1) {
-      if (count($ip4) < 1)
-        return false;
-      else
-        return $ip4;
-    } else
-      return $ip6;
-  }
   function get_server_from_url($url) {
     $start = strpos($url, '://') + 3;
     $slash = strpos($url, '/', $start);
